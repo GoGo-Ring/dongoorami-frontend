6e8d187787c7a9c100c40d856ea946a18f255e1b
junly21 <jaejoon0721@ajou.ac.kr>
2024-02-08 23:52:57 +0900

    Chore: tanstack-query 세팅 및 인스턴스 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2,
+      gcTime: 1000 * 60 * 1,
+
+      retry: 1,
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
-  return <>동구라미에요</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <section>동구라미에요</section>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
