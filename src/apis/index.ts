6fb9e7334dc894743a788b1330b14069ec04f551
junly21 <jaejoon0721@ajou.ac.kr>
2024-02-08 23:53:13 +0900

    Chore: axios 인스턴스 설정

---
+++ src/apis/index.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance, AxiosRequestConfig, Method } from "axios";
+
+const api = axios.create({
+  // TODO BaseURL, Authorization 설정
+  headers: { "Content-Type": "application/json" },
+});
+
+const HTTP_METHODS = {
+  GET: "get",
+  POST: "post",
+  PATCH: "patch",
+  PUT: "put",
+  DELETE: "delete",
+} as const;
+
+const createApiMethod =
+  (_axiosInstance: AxiosInstance, methodType: Method) =>
+  <T>(config: AxiosRequestConfig): Promise<T> => {
+    return _axiosInstance({
+      ...config,
+      method: methodType,
+    });
+  };
+
+export default {
+  get: createApiMethod(api, HTTP_METHODS.GET),
+  post: createApiMethod(api, HTTP_METHODS.POST),
+  patch: createApiMethod(api, HTTP_METHODS.PATCH),
+  put: createApiMethod(api, HTTP_METHODS.PUT),
+  delete: createApiMethod(api, HTTP_METHODS.DELETE),
+};
