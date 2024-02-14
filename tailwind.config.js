4bf90249511f1182c725a2ed01f28ccb437d28cf
seongminn <berry9901@gmail.com>
2024-02-15 02:28:24 +0900

    Style: tailwindcss color 속성 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,21 @@ module.exports = {
         sans: ['var(--font-sans)', ...fontFamily.sans],
       },
       colors: {
+        gray: {
+          50: 'var(--gray-50)',
+          100: 'var(--gray-100)',
+          150: 'var(--gray-150)',
+          200: 'var(--gray-200)',
+          250: 'var(--gray-250)',
+          300: 'var(--gray-300)',
+          400: 'var(--gray-400)',
+          500: 'var(--gray-500)',
+          600: 'var(--gray-600)',
+          700: 'var(--gray-700)',
+          800: 'var(--gray-800)',
+          900: 'var(--gray-900)',
+        },
+
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
