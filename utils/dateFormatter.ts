0a048f8fe5e6185a24aee9d8e0708b3618121240
yeonkyoung <keep.ykk@gmail.com>
2024-02-15 15:32:13 +0900

    Refactor: performanceStartDate를 일반화하여 date로 수정

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -1,7 +1,7 @@
-export const getPerformanceStartDate = (performanceStartDate: Date) => {
-  const year = performanceStartDate.getFullYear();
-  const month = String(performanceStartDate.getMonth() + 1).padStart(2, '0');
-  const day = String(performanceStartDate.getDate()).padStart(2, '0');
+export const getPerformanceStartDate = (date: Date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
 
   return `${year}-${month}-${day}`;
 };
