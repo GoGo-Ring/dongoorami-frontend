e4aa2c49fbadc0dfc4b7634e5cb4690878fb9242
ienrum <azang0506@gmail.com>
2024-02-16 11:13:49 +0900

    Feat: form 에 필요한 상수들 추가

---
+++ app/recruitment/new/constants.ts
@@ -0,0 +1,69 @@
+export const FORM_IDS = {
+  TITLE: 'title',
+  PERFORMANCE: 'performance',
+  PERFORMANCE_DATE: 'performanceDate',
+  PERFORMANCE_LOCATION: 'performanceLocation',
+  LOCATION: 'location',
+  AGE: 'age',
+  PARTICIPANTS: 'participants',
+  M: 'male',
+  F: 'femanle',
+  ANY: 'any',
+  GENDER: 'gender',
+};
+
+export const FORM_LABELS = {
+  TITLE: '제목',
+  PERFORMANCE: '공연명',
+  PERFORMANCE_DATE: '공연일',
+  PERFORMANCE_LOCATION: '공연 장소',
+  LOCATION: '지역',
+  AGE: '연령',
+  PARTICIPANTS: '인원수',
+  M: '남성',
+  F: '여성',
+  ANY: '무관',
+  GENDER: '성별',
+};
+
+export const FORM_PLACEHOLDERS = {
+  TITLE: '제목을 입력해주세요',
+  PERFORMANCE: '공연명을 입력해주세요',
+  PERFORMANCE_LOCATION: '공연 장소를 입력해주세요',
+  LOCATION: '선택',
+  PARTICIPANTS: '선택',
+  TEXTAREA: '내용을 입력해주세요',
+};
+
+export const FORM_ITEMS = {
+  LOCATION: [
+    '서울',
+    '경기',
+    '인천',
+    '대전',
+    '대구',
+    '부산',
+    '울산',
+    '세종',
+    '강원',
+    '충북',
+    '충남',
+    '경북',
+    '경남',
+    '전북',
+    '전남',
+    '제주',
+  ],
+  PARTICIPANTS: [
+    '1명',
+    '2명',
+    '3명',
+    '4명',
+    '5명',
+    '6명',
+    '7명',
+    '8명',
+    '9명',
+    '10명',
+  ],
+};
