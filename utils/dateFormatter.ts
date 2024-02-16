010b09363952a01f73b98a68ade2831a65c2bc96
yeonkyoung <keep.ykk@gmail.com>
2024-02-16 19:12:25 +0900

    Refactor: format 매개변수 추가 및 타입 지정, format 형식에 따른 내부 분기 로직 추가

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -1,7 +1,14 @@
-export const getDate = (date: Date) => {
+type DateFormat = 'yyyy-mm-dd';
+
+export const getDate = (date: Date, format: DateFormat) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
-  return `${year}-${month}-${day}`;
+  switch (format) {
+    case 'yyyy-mm-dd':
+      return `${year}-${month}-${day}`;
+    default:
+      return null;
+  }
 };
