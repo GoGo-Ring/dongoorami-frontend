2ce1cc0203d0a54907074e29e84b2cba79099a8e
yeonkyoung <keep.ykk@gmail.com>
2024-02-16 21:01:09 +0900

    Feat: 동행 필터의 constant 분리

---
+++ constants/Filter.ts
@@ -0,0 +1,22 @@
+export const GENRE_SELECTION = {
+  category: '장르',
+  options: ['콘서트', '클래식', ' 무용', '뮤지컬', '아동', '연극'],
+};
+export const STATUS_SELECTION = {
+  category: '판매 상태',
+  options: ['판매중', '판매 예정', '판매 종료'],
+};
+
+export const REGIONS = {
+  category: '지역',
+  options: [
+    '수도권(경기, 인천 포함)',
+    '강원도',
+    '충청북도/충청남도',
+    '경상북도/경상남도',
+    '전라북도/전라남도',
+    '제주도',
+  ],
+};
+
+export const MULTIPLE_SELECTION_AVAILABLE = '(중복 선택 가능)';
