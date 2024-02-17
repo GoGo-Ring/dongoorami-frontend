2665586008125b32e635f2b656d4eeae6af12fd5
junly21 <jaejoon0721@ajou.ac.kr>
2024-02-17 09:54:21 +0900

    Refactor: alt태그 로직 변경, oAuthUrl 적용 방식 변경, 버튼 타입 분리

--- app/login/_components/o-auth-button.tsx
+++ app/login/_components/o-auth-button.tsx
@@ -1,20 +1,26 @@
 import Image from 'next/image';
 
 import { buttonStyle } from '~/app/login/login-button-map';
-import { KAKAO_AUTH_URL, NAVER_AUTH_URL } from '~/app/login/loginconfig';
+import {
+  KAKAO_REST_API_KEY,
+  KAKAO_REDIRECT_URI,
+  NAVER_REST_API_KEY,
+  NAVER_REDIRECT_URI,
+} from '~/app/login/loginconfig';
 import { Button } from '~/components/button';
 
 interface OAuthButtonProps {
   domain: 'kakao' | 'naver' | 'X';
 }
 
-const urlMap: { [key: string]: string } = {
-  kakao: KAKAO_AUTH_URL,
-  naver: NAVER_AUTH_URL,
+const oauthUrl: { [key: string]: string } = {
+  kakao: `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`,
+  naver: `https://nid.naver.com/oauth2.0/authorize?client_id=${NAVER_REST_API_KEY}&redirect_uri=${NAVER_REDIRECT_URI}&response_type=code&state=STATE_STRING`,
 };
 
 const OAuthButton = ({ domain }: OAuthButtonProps) => {
-  const { bgcolor, icon, txtcolor, name, alt } = buttonStyle[domain];
+  const { bgcolor, icon, txtcolor, name } = buttonStyle[domain];
+  const alt = `${name} 로그인`;
 
   const buttonClass = `m-1 flex h-11 w-72 cursor-pointer items-center rounded-md border ${bgcolor} p-4 hover:${bgcolor}`;
 
@@ -23,7 +29,7 @@ const OAuthButton = ({ domain }: OAuthButtonProps) => {
   `;
 
   const linkAccessCode = () => {
-    window.location.href = urlMap[domain];
+    window.location.href = oauthUrl[domain];
   };
 
   return (
--- app/login/login-button-map.ts
+++ app/login/login-button-map.ts
@@ -1,10 +1,11 @@
+type Corp = 'kakao' | 'naver' | 'X' | 'google';
+
 export const buttonStyle: {
-  [key in 'kakao' | 'naver' | 'X' | 'google']: {
+  [key in Corp]: {
     name: string;
     bgcolor: string;
     icon: string;
     txtcolor: 'black' | 'white';
-    alt: string;
   };
 } = {
   kakao: {
@@ -12,27 +13,23 @@ export const buttonStyle: {
     bgcolor: 'bg-kakao',
     icon: '/kakao-icon.svg',
     txtcolor: 'black',
-    alt: '카카오 로그인',
   },
   X: {
     name: 'X',
     bgcolor: 'bg-black',
     icon: '/x-white-icon.svg',
     txtcolor: 'white',
-    alt: 'X 로그인',
   },
   naver: {
     name: '네이버',
     bgcolor: 'bg-naver',
     icon: '/naver-icon.svg',
     txtcolor: 'white',
-    alt: '네이버 로그인',
   },
   google: {
     name: '구글',
     bgcolor: 'bg-naver',
     icon: '/naver-icon.svg',
     txtcolor: 'white',
-    alt: '구글 로그인',
   },
 };
