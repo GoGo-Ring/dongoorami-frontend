9249b6571f516e253c284e882510d52519ac4b1a
yeonkyoung <keep.ykk@gmail.com>
2024-02-18 19:58:04 +0900

    Refactor: CompanionRecruitmentFilter의 내부 요소를 children으로 수정

--- app/_component/CompanionRecruitmentFilter.tsx
+++ app/_component/CompanionRecruitmentFilter.tsx
@@ -1,67 +1,15 @@
-import { Input } from '~/components/input';
-import { Label } from '~/components/label';
-import { RadioGroup, RadioGroupItem } from '~/components/radio-group';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '~/components/select';
+import { ReactNode } from 'react';
 
-const CompanionRecruitmentFilter = () => {
+interface CompanionRecruitmentFilterProps {
+  children: ReactNode;
+}
+
+const CompanionRecruitmentFilter = ({
+  children,
+}: CompanionRecruitmentFilterProps) => {
   return (
     <div>
-      <span className="font-semibold">성별</span>
-      <RadioGroup defaultValue="무관">
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem value="무관" id="무관" />
-          <Label htmlFor="무관">무관</Label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem value="남자" id="남자" />
-          <Label htmlFor="남자">남자</Label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <RadioGroupItem value="여자" id="여자" />
-          <Label htmlFor="여자">여자</Label>
-        </div>
-      </RadioGroup>
-      <div className="flex flex-col">
-        <span className="font-semibold">나이</span>
-        <div className="flex flex-row gap-1">
-          <Input type="number" className="w-16" />
-          <span className="my-auto ">~</span>
-          <Input type="number" className="w-16" />
-        </div>
-      </div>
-      <div>
-        <span className="font-semibold">교통수단</span>
-        <RadioGroup defaultValue="동행">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="동행" id="동행" />
-            <Label htmlFor="동행">동행</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="미동행" id="미동행" />
-            <Label htmlFor="미동행">미동행</Label>
-          </div>
-        </RadioGroup>
-      </div>
-      <div>
-        <span className="font-semibold">인원 수</span>
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="인원 수" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="1">1명</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+      <div className="flex flex-col">{children}</div>
     </div>
   );
 };
