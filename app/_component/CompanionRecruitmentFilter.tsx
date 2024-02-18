906645b8d494a80c85a293f951ed675228d5a782
yeonkyoung <keep.ykk@gmail.com>
2024-02-18 19:52:25 +0900

    Feat: 성별 라디오 그룹 영억 요소 추가

--- app/_component/CompanionRecruitmentFilter.tsx
+++ app/_component/CompanionRecruitmentFilter.tsx
@@ -1,5 +1,26 @@
+import { Label } from '~/components/label';
+import { RadioGroup, RadioGroupItem } from '~/components/radio-group';
+
 const CompanionRecruitmentFilter = () => {
-  return <div></div>;
+  return (
+    <div>
+      <span className="font-semibold">성별</span>
+      <RadioGroup defaultValue="무관">
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="무관" id="무관" />
+          <Label htmlFor="무관">무관</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="남자" id="남자" />
+          <Label htmlFor="남자">남자</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="여자" id="여자" />
+          <Label htmlFor="여자">여자</Label>
+        </div>
+      </RadioGroup>
+    </div>
+  );
 };
 
 export default CompanionRecruitmentFilter;
