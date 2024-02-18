079e1cfbe48ab1547b8d2c5f1bc968a2c39abaa7
yeonkyoung <keep.ykk@gmail.com>
2024-02-18 20:45:48 +0900

    Feat: RadioField 컴포넌트와 RadioItem컴포넌트 추가

--- app/_component/Filter.tsx
+++ app/_component/Filter.tsx
@@ -1,5 +1,23 @@
+import CompanionRecruitmentFilter from './CompanionRecruitmentFilter';
+import { SELECTION } from './constants';
+
 const Filter = () => {
-  return <div></div>;
+  return (
+    <CompanionRecruitmentFilter>
+      <CompanionRecruitmentFilter.RadioField
+        category={SELECTION.GENDER.category}
+        defaultValue={SELECTION.GENDER.options[0].value}
+      >
+        {SELECTION.GENDER.options.map(({ label, value }) => (
+          <CompanionRecruitmentFilter.RadioItem
+            key={label}
+            label={label}
+            value={value}
+          />
+        ))}
+      </CompanionRecruitmentFilter.RadioField>
+    </CompanionRecruitmentFilter>
+  );
 };
 
 export default Filter;
