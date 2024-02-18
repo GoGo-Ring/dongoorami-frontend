e7f549c64a70c2cc823a5fd49d7971f6c07af9b5
yeonkyoung <keep.ykk@gmail.com>
2024-02-18 15:49:15 +0900

    Refactor: 변경된상수명으로 수정

--- app/_component/Filter.tsx
+++ app/_component/Filter.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import { GENRE_SELECTION, REGIONS, STATUS_SELECTION } from '~/constants/Filter';
+import { SELECTION } from '~/constants/Filter';
 
 import PerformanceFilter from './PerformanceFilter';
 
 const Filter = () => {
   return (
     <PerformanceFilter>
-      <PerformanceFilter.ButtonField category={GENRE_SELECTION.category}>
-        {GENRE_SELECTION.options.map(option => (
+      <PerformanceFilter.ButtonField category={SELECTION.GENRE.category}>
+        {SELECTION.GENRE.options.map(option => (
           <PerformanceFilter.ButtonItem key={option} label={option} />
         ))}
       </PerformanceFilter.ButtonField>
-      <PerformanceFilter.ButtonField category={STATUS_SELECTION.category}>
-        {STATUS_SELECTION.options.map(option => (
+      <PerformanceFilter.ButtonField category={SELECTION.STATUS.category}>
+        {SELECTION.STATUS.options.map(option => (
           <PerformanceFilter.ButtonItem key={option} label={option} />
         ))}
       </PerformanceFilter.ButtonField>
-      <PerformanceFilter.CheckboxField category={REGIONS.category}>
-        {REGIONS.options.map(option => (
+      <PerformanceFilter.CheckboxField category={SELECTION.REGIONS.category}>
+        {SELECTION.REGIONS.options.map(option => (
           <PerformanceFilter.CheckboxItem key={option} label={option} />
         ))}
       </PerformanceFilter.CheckboxField>
