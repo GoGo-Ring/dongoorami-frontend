6c79825c292f0cde488ef9910a9735e304cc5bcc
yeonkyoung <keep.ykk@gmail.com>
2024-02-18 20:17:49 +0900

    Feat: GENDER 카테고리 및 옵션 추가

--- app/_component/constants.ts
+++ app/_component/constants.ts
@@ -1,1 +1,10 @@
-export const SELECTION = {};
+export const SELECTION = {
+  GENDER: {
+    category: '성별',
+    options: [
+      { value: 'irrelevant', label: '무관' },
+      { value: 'male', label: '남자' },
+      { value: 'female', label: '여자' },
+    ],
+  },
+};
