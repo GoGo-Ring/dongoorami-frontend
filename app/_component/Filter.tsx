4457adafd0641273c142d1532ce67001b91d8ffc
yeonkyoung <keep.ykk@gmail.com>
2024-02-18 15:36:59 +0900

    Feat: 장르 ButtonField 컴포넌트 추가

--- app/_component/Filter.tsx
+++ app/_component/Filter.tsx
@@ -1,7 +1,19 @@
 'use client';
 
+import { GENRE_SELECTION } from '~/constants/Filter';
+
+import PerformanceFilter from './PerformanceFilter';
+
 const Filter = () => {
-  return <></>;
+  return (
+    <PerformanceFilter>
+      <PerformanceFilter.ButtonField category={GENRE_SELECTION.category}>
+        {GENRE_SELECTION.options.map(option => (
+          <PerformanceFilter.ButtonItem key={option} label={option} />
+        ))}
+      </PerformanceFilter.ButtonField>
+    </PerformanceFilter>
+  );
 };
 
 export default Filter;
