4c3bfd1b129c5e41ad73cfe6b2435d602ec89e0c
ienrum <137467530+ienrum@users.noreply.github.com>
2024-02-21 15:14:50 +0900

    Feat: [#25] 폼 상태 관리 및 유효성 검사를 위한 useForm 구현 (#32)
    * Feat: useForm 을 구현합니다.
    
    * Refactor: 이벤트 타입에 htmlSelect 요소 추가
    
    * Refactor: useForm 타입 리팩토링
    
    컴포넌트의 props 형식에서 함수 형식으로 변경
    
    FormEvent to ChangeEvent
    SyntheticEvent to FromEvent
    
    getFieldProps 가 공용으로 사용되지않아, 제거
    
    * Refactor: 함수 타입 지정
    
    중복되는 타입 공통으로 지정
    
    * Refactor: HTMLFormElement 타입 지정

---
+++ hooks/useForm.ts
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+type HandleChange = <
+  E extends HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement,
+>(
+  e: React.ChangeEvent<E>,
+) => void;
+
+type HandleSubmit = (e: React.FormEvent<HTMLFormElement>) => void;
+
+type UseForm = <T extends Record<string, string>>(
+  initialValues: T,
+  onSubmit: (values: T) => void,
+) => {
+  values: T;
+  errors: Record<keyof T, boolean>;
+  setValues: React.Dispatch<React.SetStateAction<T>>;
+  handleChange: HandleChange;
+  handleSubmit: HandleSubmit;
+};
+
+const useForm: UseForm = <T>(
+  initialValues: T,
+  onSubmit: (values: T) => void,
+) => {
+  const [values, setValues] = useState<T>(initialValues);
+  const [errors, setErrors] = useState<Record<keyof T, boolean>>(
+    {} as Record<keyof T, boolean>,
+  );
+
+  const handleChange: HandleChange = e => {
+    const { id, value } = e.currentTarget;
+
+    if (!value) {
+      setErrors({ ...errors, [id]: true });
+
+      return;
+    }
+    setValues({ ...values, [id]: value });
+    setErrors({ ...errors, [id]: false });
+  };
+
+  const handleSubmit: HandleSubmit = e => {
+    e.preventDefault();
+    onSubmit(values);
+  };
+
+  return {
+    values,
+    errors,
+    setValues,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useForm;
