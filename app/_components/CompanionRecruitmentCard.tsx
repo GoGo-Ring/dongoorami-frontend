c8e8c5d4958175795bfa4dd163491de0827665b6
yeonkyoung <keep.ykk@gmail.com>
2024-02-26 14:17:12 +0900

    Refactor: [#41] 동행 구함 카드 컴포넌트 내부에 props 전달 (#42)
    * Feat: date 형식으로 'yyyy.mm.dd'추가
    
    * Refactor: 동행 구함 카드 컴포넌트 내부에 props를 contextAPI로 전달

--- app/_components/CompanionRecruitmentCard.tsx
+++ app/_components/CompanionRecruitmentCard.tsx
@@ -1,13 +1,31 @@
+'use client';
+import { useContext, createContext } from 'react';
+
 import { Badge } from '~/components/badge';
+import { getDate } from '~/utils/dateFormatter';
+
+interface CompanionRecruitmentCardProps {
+  title: string;
+  concertName: string;
+  userId: string;
+  gender: string;
+  personCount: number;
+  viewCount: number;
+  commentsCount: number;
+  date: Date;
+  status: '모집 중' | '모집 종료';
+}
+
+const CompanionRecruitmentCardContext = createContext(
+  {} as CompanionRecruitmentCardProps,
+);
 
 const Title = () => {
+  const { title } = useContext(CompanionRecruitmentCardContext);
+
   return (
     <div className="text-gray-700 ">
-      <h3 className="line-clamp-3">
-        {
-          '글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목글제목'
-        }
-      </h3>
+      <h3 className="line-clamp-3">{title}</h3>
     </div>
   );
 };
@@ -27,12 +45,16 @@ const ConditionItem = ({ label, contents }: ConditionItemProps) => {
 };
 
 const ContentField = () => {
+  const { concertName, gender, personCount } = useContext(
+    CompanionRecruitmentCardContext,
+  );
+
   return (
     <div className="flex w-[156px] flex-col gap-1 pb-1">
       <div className="flex flex-col">
-        <ConditionItem label="공연명" contents="concertName" />
-        <ConditionItem label="성별" contents="gender" />
-        <ConditionItem label="인원 수" contents={1} />
+        <ConditionItem label="공연명" contents={concertName} />
+        <ConditionItem label="성별" contents={gender} />
+        <ConditionItem label="인원 수" contents={personCount} />
       </div>
     </div>
   );
@@ -53,24 +75,35 @@ const IconWithCountItem = ({ icon, count }: IconWithCountItemProps) => {
 };
 
 const IconField = () => {
+  const { viewCount, commentsCount } = useContext(
+    CompanionRecruitmentCardContext,
+  );
+
   return (
     <div className="flex flex-row justify-end gap-2 text-sm text-gray-300">
-      <IconWithCountItem icon={'icon'} count={1} />
-      <IconWithCountItem icon={'icon'} count={1} />
+      <IconWithCountItem icon={'icon'} count={viewCount} />
+      <IconWithCountItem icon={'icon'} count={commentsCount} />
     </div>
   );
 };
 
 const UserId = () => {
-  return <span className="truncate text-sm text-gray-400">{'userId'}</span>;
+  const { userId } = useContext(CompanionRecruitmentCardContext);
+
+  return <span className="truncate text-sm text-gray-400">{userId}</span>;
 };
 
 const CreatedDate = () => {
-  return <span className="text-xs text-gray-300">{'date'}</span>;
+  const { date } = useContext(CompanionRecruitmentCardContext);
+  const formattedDate = getDate(date, 'yyyy.mm.dd');
+
+  return <span className="text-xs text-gray-300">{formattedDate}</span>;
 };
 
 const BadgeItem = () => {
-  return <Badge className="h-fit flex-shrink-0">{'status'}</Badge>;
+  const { status } = useContext(CompanionRecruitmentCardContext);
+
+  return <Badge className="h-fit flex-shrink-0">{status}</Badge>;
 };
 
 const FooterField = () => {
@@ -85,37 +118,37 @@ const FooterField = () => {
   );
 };
 
-// interface CompanionRecruitmentCardProps {
-//   title: string;
-//   concertName: string;
-//   userId: string;
-//   gender: string;
-//   personCount: number;
-//   viewCount: number;
-//   commentsCount: number;
-//   date: string;
-//   status: '모집 중' | '모집 종료';
-//   updatedAt: Date;
-// }
-
-const CompanionRecruitmentCard = () => {
-  //   {
-  //   title,
-  //   concertName,
-  //   userId,
-  //   gender,
-  //   personCount,
-  //   viewCount,
-  //   commentsCount,
-  //   date,
-  //   status,
-  // }: CompanionRecruitmentCardProps
+const CompanionRecruitmentCard = ({
+  title,
+  concertName,
+  userId,
+  gender,
+  personCount,
+  viewCount,
+  commentsCount,
+  date,
+  status,
+}: CompanionRecruitmentCardProps) => {
   return (
     <div className=" w-[204px] rounded-lg border p-6">
-      <Title />
-      <ContentField />
-      <IconField />
-      <FooterField />
+      <CompanionRecruitmentCardContext.Provider
+        value={{
+          title,
+          concertName,
+          userId,
+          gender,
+          personCount,
+          viewCount,
+          commentsCount,
+          date,
+          status,
+        }}
+      >
+        <Title />
+        <ContentField />
+        <IconField />
+        <FooterField />
+      </CompanionRecruitmentCardContext.Provider>
     </div>
   );
 };
