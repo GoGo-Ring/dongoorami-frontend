d20c9afdd4a9e1a141e6ccaa27bc694cda2afedd
yeonkyoung <keep.ykk@gmail.com>
2024-02-27 03:58:02 +0900

    Chore: 파일 삭제

--- app/_components/CompanionRecruitmentFilter.tsx
+++ app/_components/CompanionRecruitmentFilter.tsx
@@ -1,78 +1,9 @@
 'use client';
 
-import { useRef } from 'react';
+import CompanionRecruitmentFilter from './companion-recruitment-filter';
 
-import CompanionRecruitmentFilterContainer from './CompanionRecruitmentFilterContainer';
-import { SELECTION } from './constants';
-
-const CompanionRecruitmentFilter = () => {
-  const defaultValues = Object.values(SELECTION).reduce(
-    (acc, { category, options }) => {
-      return { ...acc, [category]: options[0].value.toString() };
-    },
-    {},
-  );
-
-  const ref = useRef<Record<string, string>>(defaultValues);
-
-  const setRef = (category: string, value: string) => {
-    ref.current[category] = value;
-  };
-
-  return (
-    <CompanionRecruitmentFilterContainer>
-      <CompanionRecruitmentFilterContainer.RadioField
-        category={SELECTION.GENDER.category}
-        defaultValue={SELECTION.GENDER.options[0].value}
-        handleState={setRef}
-      >
-        {SELECTION.GENDER.options.map(({ label, value }) => (
-          <CompanionRecruitmentFilterContainer.RadioItem
-            key={label}
-            label={label}
-            value={value}
-          />
-        ))}
-      </CompanionRecruitmentFilterContainer.RadioField>
-      <CompanionRecruitmentFilterContainer.InputField
-        category={'나이'}
-        defaultValue={[
-          SELECTION.AGE.options[0].value,
-          SELECTION.AGE.options[1].value,
-        ]}
-      >
-        <CompanionRecruitmentFilterContainer.InputItem name="min" />
-        <CompanionRecruitmentFilterContainer.InputItem name="max" />
-      </CompanionRecruitmentFilterContainer.InputField>
-      <CompanionRecruitmentFilterContainer.RadioField
-        category={SELECTION.TRANSPORTATION.category}
-        defaultValue={SELECTION.TRANSPORTATION.options[0].value}
-        handleState={setRef}
-      >
-        {SELECTION.TRANSPORTATION.options.map(({ label, value }) => (
-          <CompanionRecruitmentFilterContainer.RadioItem
-            key={label}
-            label={label}
-            value={value}
-          />
-        ))}
-      </CompanionRecruitmentFilterContainer.RadioField>
-      <CompanionRecruitmentFilterContainer.SelectField
-        category={SELECTION.PERSON_COUNT.category}
-        defaultValue={SELECTION.PERSON_COUNT.options[0].value}
-        placeholder={SELECTION.PERSON_COUNT.options[0].label}
-        handleState={setRef}
-      >
-        {SELECTION.PERSON_COUNT.options.map(({ label, value }) => (
-          <CompanionRecruitmentFilterContainer.SelectOptionItem
-            key={label}
-            label={label}
-            value={value}
-          />
-        ))}
-      </CompanionRecruitmentFilterContainer.SelectField>
-    </CompanionRecruitmentFilterContainer>
-  );
+const Filter = () => {
+  return <CompanionRecruitmentFilter />;
 };
 
-export default CompanionRecruitmentFilter;
+export default Filter;
