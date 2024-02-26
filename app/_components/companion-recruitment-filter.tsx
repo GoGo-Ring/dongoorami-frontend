4dbd91f4d33af72405d744243a08c9ba2b869e28
yeonkyoung <keep.ykk@gmail.com>
2024-02-27 03:57:40 +0900

    Feat: SelectField 추가

--- app/_components/companion-recruitment-filter.tsx
+++ app/_components/companion-recruitment-filter.tsx
@@ -8,6 +8,7 @@ import { SEARCH, SELECTION } from '~/constants/filterField';
 import CheckboxSelectField from './filter-field/checkbox-select';
 import InputField from './filter-field/input-field';
 import RadioField from './filter-field/radio-field';
+import SelectField from './filter-field/select-field';
 
 export interface refType {
   gender: string;
@@ -32,6 +33,7 @@ const CompanionRecruitmentFilter = () => {
     SELECTION.TRANSPORTATION.options[0].value,
   );
   const ageRef = useRef([20, 30]);
+  const personCountRef = useRef(1);
 
   const handle = () => {};
 
@@ -56,6 +58,13 @@ const CompanionRecruitmentFilter = () => {
         category={SELECTION.AGE.category}
         defaultValues={SELECTION.AGE.options}
         ref={ageRef}
+      />
+      <SelectField
+        category={SELECTION.PERSON_COUNT.category}
+        options={SELECTION.PERSON_COUNT.options}
+        defaultValue={SELECTION.PERSON_COUNT.options[0].value}
+        placeholder={SELECTION.PERSON_COUNT.options[0].label}
+        ref={personCountRef}
       />
       <Button variant="outline" onClick={handle}>
         {SEARCH}
