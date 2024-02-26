87ba02bc2fe3c467a1863f754a0a7080a0343722
junly21 <jaejoon0721@ajou.ac.kr>
2024-02-27 00:10:58 +0900

    Fix: 컴포넌트 렌더링 완료 전 router 이동 useEffect처리

--- app/oauth/page.tsx
+++ app/oauth/page.tsx
@@ -1,22 +1,24 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/router';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const OAuth = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const accessToken = searchParams.get('accessToken');
-  const refreshToken = searchParams.get('refreshToken');
-  const isFirstLogin = searchParams.get('isFirstLogin');
+  useEffect(() => {
+    const accessToken = searchParams.get('accessToken');
+    const refreshToken = searchParams.get('refreshToken');
+    const isFirstLogin = searchParams.get('isFirstLogin');
 
-  if (accessToken && refreshToken) {
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
-  }
+    if (accessToken && refreshToken) {
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
+    }
 
-  isFirstLogin ? router.push('/register') : router.push('/');
+    isFirstLogin === 'true' ? router.push('/register') : router.push('/');
+  });
 };
 
 export default OAuth;
