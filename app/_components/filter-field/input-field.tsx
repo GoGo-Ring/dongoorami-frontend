52b0c0f2a69cfde7b20e6af581164d145cc5c200
yeonkyoung <keep.ykk@gmail.com>
2024-02-27 03:47:05 +0900

    Refactor: 입력 필드 값 변경시 ref 최신화

--- app/_components/filter-field/input-field.tsx
+++ app/_components/filter-field/input-field.tsx
@@ -6,6 +6,7 @@ import { Input } from '~/components/input';
 interface InputItemProps {
   type: string;
   defaultValue: number;
+  onChange: () => void;
 }
 
 interface InputFieldProps {
@@ -14,32 +15,70 @@ interface InputFieldProps {
 }
 
 const InputItem = forwardRef<HTMLInputElement, InputItemProps>(
-  ({ type, defaultValue }, ref) => {
+  ({ type, defaultValue, onChange }, ref) => {
     return (
-      <Input type={type} className="w-16" min={0} max={100} ref={ref}>
-        {defaultValue}
-      </Input>
+      <Input
+        defaultValue={defaultValue}
+        type={type}
+        className="w-16"
+        min={0}
+        max={100}
+        ref={ref}
+        onChange={onChange}
+      />
     );
   },
 );
 
 InputItem.displayName = 'InputItem';
 
-const InputField = ({ category, defaultValues }: InputFieldProps) => {
-  const minRef = useRef<HTMLInputElement>(null);
-  const maxRef = useRef<HTMLInputElement>(null);
-  const [minValue, maxValue] = defaultValues;
+const InputField = forwardRef<number[], InputFieldProps>(
+  ({ category, defaultValues }, ref) => {
+    const minRef = useRef<HTMLInputElement>(null);
+    const maxRef = useRef<HTMLInputElement>(null);
+    const [minValue, maxValue] = defaultValues;
 
-  return (
-    <>
-      <span className="font-semibold">{category}</span>
-      <div className="flex flex-row gap-1">
-        <InputItem defaultValue={minValue} type="number" ref={minRef} />
-        <span className="my-auto ">~</span>
-        <InputItem defaultValue={maxValue} type="number" ref={maxRef} />
-      </div>
-    </>
-  );
-};
+    const onChange = () => {
+      if (!ref) {
+        return;
+      }
+      if (typeof ref === 'function') {
+        return;
+      }
+      if (!ref.current || !minRef.current || !maxRef.current) {
+        return;
+      }
+      if (typeof ref.current === 'function') {
+        return;
+      }
+      ref.current = [
+        parseInt(minRef.current.value),
+        parseInt(maxRef.current.value),
+      ];
+    };
 
+    return (
+      <>
+        <span className="font-semibold">{category}</span>
+        <div className="flex flex-row gap-1">
+          <InputItem
+            defaultValue={minValue}
+            type="number"
+            ref={minRef}
+            onChange={onChange}
+          />
+          <span className="my-auto ">~</span>
+          <InputItem
+            defaultValue={maxValue}
+            type="number"
+            ref={maxRef}
+            onChange={onChange}
+          />
+        </div>
+      </>
+    );
+  },
+);
+
+InputField.displayName = 'InputField';
 export default InputField;
