d64b1e4e472f65a3de8547cfd5c99c7c81f2aa17
yeonkyoung <keep.ykk@gmail.com>
2024-02-27 02:39:36 +0900

    Refactor: children을 RadioField로 변경

--- app/_components/companion-recruitment-filter.tsx
+++ app/_components/companion-recruitment-filter.tsx
@@ -1,21 +1,33 @@
 'use client';
-import { ReactNode } from 'react';
+
+import { useRef } from 'react';
 
 import { Button } from '~/components/button';
 
-import { SEARCH } from './constants';
+import { SEARCH, SELECTION } from './constants';
+import RadioField from './filter-field/radio-field';
 
-interface CompanionRecruitmentFilterProps {
-  children: ReactNode;
+export interface refType {
+  gender: string;
+  region: string[];
+  age: number[];
+  transportation: string;
+  personCount: number;
 }
 
-const CompanionRecruitmentFilter = ({
-  children,
-}: CompanionRecruitmentFilterProps) => {
+const CompanionRecruitmentFilter = () => {
+  const radioRef = useRef<string>(SELECTION.GENDER.options[0].value);
+
+  const handle = () => {};
+
   return (
     <div className="flex w-[260px] flex-col gap-6 px-3">
-      {children}
-      <Button variant="outline" type="submit">
+      <RadioField
+        category={SELECTION.GENDER.category}
+        options={SELECTION.GENDER.options}
+        ref={radioRef}
+      />
+      <Button variant="outline" onClick={handle}>
         {SEARCH}
       </Button>
     </div>
