898ff4064b4f0d0c6f23f7d7c24ee17f83672549
seongminn <berry9901@gmail.com>
2024-02-27 13:32:36 +0900

    Refactor: Icon size type 변경

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -3,19 +3,26 @@ import { ComponentProps } from 'react';
 import { cn } from '~/libs/utils';
 
 import Icons, { IconNames } from './icons';
+import { IconSize, getSize } from './utils';
 
 interface IconProps extends ComponentProps<'svg'> {
   iconName: IconNames;
-  size?: number;
+  size?: IconSize | number;
 }
 
-const Icon = ({ iconName, size = 16, className, ...props }: IconProps) => {
+const Icon = ({
+  iconName,
+  size = 'medium',
+  className,
+  ...props
+}: IconProps) => {
   const Component = Icons[iconName];
+  const iconSize = getSize(size);
 
   return (
     <Component
-      width={size}
-      height={size}
+      width={iconSize}
+      height={iconSize}
       className={cn('fill-foreground', className)}
       {...props}
     />
