5c9cb375becfe26ec9ee6b2a90d6cd7468bda446
junly21 <jaejoon0721@ajou.ac.kr>
2024-02-27 12:40:18 +0900

    Style: 스피너, 스켈레톤 추가 및 로그인화면 레이아웃 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ const Login = () => {
   const router = useRouter();
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <div className="flex-column h-96 w-96 min-w-96 space-y-8 p-1 ">
         <h1 className="text-center text-5xl">동구라미 로고</h1>
         <div className="rounded-md border border-slate-200 p-8">
