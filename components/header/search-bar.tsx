b6c84c439dcaeac15bb194e6ad83cd585cbfd421
seongminn <berry9901@gmail.com>
2024-02-28 09:48:04 +0900

    Refactor: 아이콘 변경 및 웹 접근성을 고려한 속성 추가

--- components/header/search-bar.tsx
+++ components/header/search-bar.tsx
@@ -1,8 +1,7 @@
-// import { cva } from 'class-variance-authority';
-import { Search } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useRef } from 'react';
 
+import Icon from '../icon';
 import { Input } from '../input';
 
 const SearchBar = () => {
@@ -27,11 +26,12 @@ const SearchBar = () => {
       onSubmit={handleSubmit}
       className="relative flex h-10 w-full items-center gap-2 rounded-md border border-input bg-background px-2.5 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
     >
-      <Search />
+      <Icon iconName="search" aria-label="검색" />
       <Input
         ref={ref}
         name="search"
         type="text"
+        role="search"
         placeholder="검색어를 입력해주세요."
         className="my-0 h-auto rounded-sm border-none p-0 placeholder-shown:text-ellipsis focus-visible:ring-0 focus-visible:ring-offset-0"
       />
