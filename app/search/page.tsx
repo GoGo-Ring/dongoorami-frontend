44d150288c0be37216cef4482b1ca8c4765f1126
yeonkyoung <keep.ykk@gmail.com>
2024-03-01 00:22:30 +0900

    Feat: 동행 구함 mock 데이터 추가

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,5 @@
 import PerformanceInfoCard from './_component/PerformanceInfoCard';
+import CompanionRecruitmentCard from '../_components/CompanionRecruitmentCard';
 import FilterTabs from '../_components/filter-tabs';
 
 const Page = () => {
@@ -29,6 +30,69 @@ const Page = () => {
     },
   ];
 
+  const accompanyList = [
+    {
+      _id: 11,
+      title: '콘서트 동행하실 분 구해요',
+      concertName: '아이유 콘서트',
+      userId: '작성자',
+      gender: '무관',
+      personCount: 1,
+      viewCount: 3,
+      commentsCount: 3,
+      date: new Date(),
+      status: '모집 중' as const,
+    },
+    {
+      _id: 12,
+      title: '콘서트 동행하실 분 구해요',
+      concertName: '아이유 콘서트',
+      userId: '작성자',
+      gender: '무관',
+      personCount: 1,
+      viewCount: 3,
+      commentsCount: 3,
+      date: new Date(),
+      status: '모집 중' as const,
+    },
+    {
+      _id: 13,
+      title: '콘서트 동행하실 분 구해요',
+      concertName: '아이유 콘서트',
+      userId: '작성자',
+      gender: '무관',
+      personCount: 1,
+      viewCount: 3,
+      commentsCount: 3,
+      date: new Date(),
+      status: '모집 중' as const,
+    },
+    {
+      _id: 14,
+      title: '콘서트 동행하실 분 구해요',
+      concertName: '아이유 콘서트',
+      userId: '작성자',
+      gender: '무관',
+      personCount: 1,
+      viewCount: 3,
+      commentsCount: 3,
+      date: new Date(),
+      status: '모집 중' as const,
+    },
+    {
+      _id: 15,
+      title: '콘서트 동행하실 분 구해요',
+      concertName: '아이유 콘서트',
+      userId: '작성자',
+      gender: '무관',
+      personCount: 1,
+      viewCount: 3,
+      commentsCount: 3,
+      date: new Date(),
+      status: '모집 중' as const,
+    },
+  ];
+
   return (
     <div className="flex">
       <FilterTabs />
@@ -42,6 +106,35 @@ const Page = () => {
               title={title}
               facilityName={facilityName}
               startDate={startDate}
+              status={status}
+            />
+          ),
+        )}
+      </div>
+      <div className="mx-auto grid grid-cols-3 gap-4">
+        {accompanyList.map(
+          ({
+            _id,
+            title,
+            concertName,
+            userId,
+            gender,
+            personCount,
+            viewCount,
+            commentsCount,
+            date,
+            status,
+          }) => (
+            <CompanionRecruitmentCard
+              key={_id}
+              title={title}
+              concertName={concertName}
+              userId={userId}
+              gender={gender}
+              personCount={personCount}
+              viewCount={viewCount}
+              commentsCount={commentsCount}
+              date={date}
               status={status}
             />
           ),
