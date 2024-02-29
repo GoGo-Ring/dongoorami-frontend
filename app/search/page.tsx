2e2935672c8072b6b462e750a46e1c97ad68e957
yeonkyoung <keep.ykk@gmail.com>
2024-03-01 00:17:46 +0900

    Feat: 공연 정보 mock 데이터 리스트 추가

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,49 @@
-const Page = () => {};
+import PerformanceInfoCard from './_component/PerformanceInfoCard';
+
+const Page = () => {
+  const performList = [
+    {
+      _id: 0,
+      posterSrc: '',
+      title: '제목',
+      facilityName: '장소',
+      startDate: new Date(),
+      status: '공연 예정' as const,
+    },
+    {
+      _id: 1,
+      posterSrc: '',
+      title: '제목22',
+      facilityName: '장소',
+      startDate: new Date(),
+      status: '공연 예정' as const,
+    },
+    {
+      _id: 2,
+      posterSrc: '',
+      title: '제목33',
+      facilityName: '장소',
+      startDate: new Date(),
+      status: '공연 예정' as const,
+    },
+  ];
+
+  return (
+    <div className="flex">
+      {performList.map(
+        ({ _id, posterSrc, title, facilityName, startDate, status }) => (
+          <PerformanceInfoCard
+            key={_id}
+            posterSrc={posterSrc}
+            title={title}
+            facilityName={facilityName}
+            startDate={startDate}
+            status={status}
+          />
+        ),
+      )}
+    </div>
+  );
+};
 
 export default Page;
