752c19c1b14392fce8f2f03a9767611d6e529cc3
yeonkyoung <keep.ykk@gmail.com>
2024-03-02 23:50:48 +0900

    Refactor: mock 데이터 삭제, performancesQuery 추가

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,36 +1,14 @@
+'use client';
+
 import { Button } from '~/components/button';
 
 import PerformanceInfoCard from './_component/PerformanceInfoCard';
 import CompanionRecruitmentCard from '../_components/CompanionRecruitmentCard';
 import FilterTabs from '../_components/filter-tabs';
+import { usePerformances } from '../queries';
 
 const Page = () => {
-  const performList = [
-    {
-      _id: 0,
-      posterSrc: '',
-      title: '제목',
-      facilityName: '장소',
-      startDate: new Date(),
-      status: '공연 예정' as const,
-    },
-    {
-      _id: 1,
-      posterSrc: '',
-      title: '제목22',
-      facilityName: '장소',
-      startDate: new Date(),
-      status: '공연 예정' as const,
-    },
-    {
-      _id: 2,
-      posterSrc: '',
-      title: '제목33',
-      facilityName: '장소',
-      startDate: new Date(),
-      status: '공연 예정' as const,
-    },
-  ];
+  const performancesQuery = usePerformances();
 
   const accompanyList = [
     {
@@ -104,13 +82,15 @@ const Page = () => {
         <div className="flex flex-col gap-6">
           <span className="font-semibold">공연</span>
           <div className="mx-auto grid grid-cols-3">
-            {performList.map(
-              ({ _id, posterSrc, title, facilityName, startDate, status }) => (
+            {performancesQuery?.data?.map(
+              ({ id, posterSrc, title, facilityName, startDate, status }) => (
                 <PerformanceInfoCard
-                  key={_id}
+                  key={id}
                   posterSrc={posterSrc}
                   title={title}
                   facilityName={facilityName}
+                  // startDate={new Date()}
+                  // status={'공연 중'}
                   startDate={startDate}
                   status={status}
                 />
