ae339d850f0084cfd033ccbcb6734ee14f0e9096
yeonkyoung <keep.ykk@gmail.com>
2024-03-02 23:46:27 +0900

    Feat: msw의 setupWorker에 등록하기 위해 handlers에 performances handler 추가

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,4 +1,5 @@
 import accompany from './accompany';
 import members from './members';
+import performances from './performances';
 
-export const handlers = [...members, ...accompany];
+export const handlers = [...members, ...accompany, ...performances];
