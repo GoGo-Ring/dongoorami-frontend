328806005ad455902a680be1ac2194af1df63baf
yeonkyoung <keep.ykk@gmail.com>
2024-03-03 00:35:32 +0900

    Feat: useCompanion 추가

--- app/queries.ts
+++ app/queries.ts
@@ -1,10 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 
+import getCompanion from '~/apis/companion';
+
 import getPerformances from '../apis/performance';
 
 export const usePerformances = () => {
   return useQuery({
     queryKey: ['performances'],
     queryFn: getPerformances,
+  });
+};
+
+export const useCompanion = () => {
+  return useQuery({
+    queryKey: ['companions'],
+    queryFn: getCompanion,
   });
 };
