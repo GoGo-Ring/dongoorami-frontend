18cc408e83df51dfc55cd3bd7c3b64ed53480d3d
yeonkyoung <keep.ykk@gmail.com>
2024-03-04 17:58:04 +0900

    Feat: rate에 따라 filledstar의 color 렌더링

--- app/review/_components/star-rating.tsx
+++ app/review/_components/star-rating.tsx
@@ -1,4 +1,6 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useMemo, useState } from 'react';
+
+import Icon from '~/components/icon';
 
 interface InputRatingProps {
   width?: number;
@@ -18,8 +20,101 @@ const InputRating = ({ onChange }: InputRatingProps) => {
     </div>
   );
 };
+
+interface FilledStarProps {
+  filled: 'half' | 'full' | 'none';
+}
+
+const FilledStar = ({ filled }: FilledStarProps) => {
+  if (filled === 'full') {
+    return <Icon iconName="filled-star" className="full" />;
+  }
+  if (filled === 'half') {
+    return <Icon iconName="filled-star" className="half" />;
+  }
+
+  return <Icon iconName="filled-star" className="none" />;
+};
+
+enum StarState {
+  None = 'none',
+  Half = 'half',
+  Full = 'full',
+}
+
 const StarRating = () => {
-  <InputRating onChange={onChange} width={width} />;
+  const [rate, setRate] = useState(0);
+  const star = useMemo(() => {
+    const rating = [
+      StarState.None,
+      StarState.None,
+      StarState.None,
+      StarState.None,
+      StarState.None,
+    ];
+
+    if (rate === 0) {
+      return rating;
+    }
+
+    return rating.map((_, index) => {
+      if (rate === 0.5 && index === 0) {
+        return StarState.Half;
+      }
+      if (rate === 1 && index === 0) {
+        return StarState.Full;
+      }
+      if (rate === 1.5 && index === 1) {
+        return StarState.Half;
+      }
+      if (rate === 2 && index === 1) {
+        return StarState.Full;
+      }
+      if (rate === 2.5 && index === 2) {
+        return StarState.Half;
+      }
+      if (rate === 3 && index === 2) {
+        return StarState.Full;
+      }
+      if (rate === 3.5 && index === 3) {
+        return StarState.Half;
+      }
+      if (rate === 4 && index === 3) {
+        return StarState.Full;
+      }
+      if (rate === 4.5 && index === 4) {
+        return StarState.Half;
+      }
+      if (rate === 5 && index === 4) {
+        return StarState.Full;
+      }
+      if (index < rate) {
+        return StarState.Full;
+      }
+
+      return StarState.None;
+    });
+  }, [rate]);
+  const width = 132;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+
+    setRate(parseFloat(value));
+  };
+
+  return (
+    <div className="flex w-fit flex-col">
+      <InputRating onChange={onChange} width={width} />;
+      <div className={'flex w-36 justify-end'}>
+        <div className={'flex w-[132px] justify-between'}>
+          {star?.map((value, index) => (
+            <FilledStar key={`star_${index}`} filled={value} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StarRating;
