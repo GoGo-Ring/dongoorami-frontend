1737e8f041a6855bcb18edf3e4d0f555c1e7f715
yeonkyoung <keep.ykk@gmail.com>
2024-03-04 17:55:35 +0900

    Feat: InputRating 컴포넌트 추가

--- app/review/_components/star-rating.tsx
+++ app/review/_components/star-rating.tsx
@@ -1,5 +1,25 @@
+import { ChangeEvent } from 'react';
+
+interface InputRatingProps {
+  width?: number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+const InputRating = ({ onChange }: InputRatingProps) => {
+  return (
+    <div>
+      <input
+        type="range"
+        onChange={onChange}
+        step={0.5}
+        min={0}
+        max={5}
+        className={'w-36'}
+      />
+    </div>
+  );
+};
 const StarRating = () => {
-  return <></>;
+  <InputRating onChange={onChange} width={width} />;
 };
 
 export default StarRating;
