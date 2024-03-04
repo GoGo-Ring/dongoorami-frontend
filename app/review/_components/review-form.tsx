ff5b10a96216b680d5a2b87d71d8306d7edf4a32
yeonkyoung <keep.ykk@gmail.com>
2024-03-04 19:21:27 +0900

    Feat: ref 추가

--- app/review/_components/review-form.tsx
+++ app/review/_components/review-form.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { forwardRef, useState } from 'react';
 
 import { Button } from '~/components/button';
 import {
@@ -15,42 +15,47 @@ import StarRating from './star-rating';
 interface ReviewFormProps {
   username: string;
 }
-const ReviewForm = ({ username }: ReviewFormProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+const ReviewForm = forwardRef<HTMLTextAreaElement, ReviewFormProps>(
+  ({ username }, ref) => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  const [rate, setRate] = useState(0);
+    const [rate, setRate] = useState(0);
 
-  return (
-    <Collapsible
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      className="w-[350px] space-y-2"
-    >
-      <div className="flex items-center justify-between space-x-4 px-4">
-        <span className="text-sm font-semibold">{username}</span>
-        <CollapsibleTrigger asChild>
-          <Button variant="ghost" size="sm">
-            <Icon
-              iconName="chevron-down"
-              className={cn('transition duration-500', {
-                'rotate-180 transform': isOpen,
-              })}
-            />
-            <span className="sr-only">Toggle</span>
-          </Button>
-        </CollapsibleTrigger>
-      </div>
-      <CollapsibleContent className="space-y-2">
-        <div className="flex justify-center ">
-          <StarRating rate={rate} setRate={setRate} />
+    return (
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-[350px] space-y-2"
+      >
+        <div className="flex items-center justify-between space-x-4 px-4">
+          <span className="text-sm font-semibold">{username}</span>
+          <CollapsibleTrigger asChild>
+            <Button variant="ghost" size="sm">
+              <Icon
+                iconName="chevron-down"
+                className={cn('transition duration-500', {
+                  'rotate-180 transform': isOpen,
+                })}
+              />
+              <span className="sr-only">Toggle</span>
+            </Button>
+          </CollapsibleTrigger>
         </div>
-        <Textarea
-          className="resize-none"
-          placeholder="동행자, 동행 상황에 대한 솔직한 리뷰를 남겨주세요."
-        />
-      </CollapsibleContent>
-    </Collapsible>
-  );
-};
+        <CollapsibleContent className="space-y-2">
+          <div className="flex justify-center ">
+            <StarRating rate={rate} setRate={setRate} />
+          </div>
+          <Textarea
+            ref={ref}
+            className="resize-none"
+            placeholder="동행자, 동행 상황에 대한 솔직한 리뷰를 남겨주세요."
+          />
+        </CollapsibleContent>
+      </Collapsible>
+    );
+  },
+);
+
+ReviewForm.displayName = 'ReviewForm';
 
 export default ReviewForm;
