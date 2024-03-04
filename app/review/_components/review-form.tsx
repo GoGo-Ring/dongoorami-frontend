05d865b1a4f78255840aef27aaf58b2e5e0feb25
yeonkyoung <keep.ykk@gmail.com>
2024-03-04 11:16:13 +0900

    Feat: Collapsible 컴포넌트 배치

--- app/review/_components/review-form.tsx
+++ app/review/_components/review-form.tsx
@@ -1,5 +1,28 @@
+import { Button } from '~/components/button';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '~/components/collapsible';
+
 const ReviewForm = () => {
-  return <></>;
+  return (
+    <Collapsible className="w-[350px] space-y-2">
+      <div className="flex items-center justify-between space-x-4 px-4">
+        <span className="text-sm font-semibold">{}</span>
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+      <CollapsibleContent className="space-y-2">
+        <div className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm">
+          @radix-ui/colors
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  );
 };
 
 export default ReviewForm;
