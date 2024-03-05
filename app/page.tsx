4db008bbdfbf7bbab2e3364e465faa5803267d54
junly21 <jaejoon0721@ajou.ac.kr>
2024-03-05 19:47:04 +0900

    Style: 캐러셀 레이아웃 생성

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import { Button } from '~/components/button';
 const Page = () => {
   return (
     <div>
-      <div className="bg-red-500">test</div>
-      <div className="bg-blue-500">test</div>
+      <div className="flex flex-col gap-y-6 border border-black py-6 ">
+        <div className="flex h-[426px] justify-center bg-yellow-500">
+          <div className="h-full w-[900px] bg-yellow-300"></div>
+        </div>
+        <div className="h-[12px] bg-green-500"></div>
+      </div>
       <Button>test</Button>
     </div>
   );
