94e735fd099e9ae8e7e65f5b12d7ca5675e4cf4c
yeonkyoung <keep.ykk@gmail.com>
2024-03-05 22:49:31 +0900

    Refactor: setOption, fieldName props추가

--- app/_components/filter-field/radio-field.tsx
+++ app/_components/filter-field/radio-field.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 
 import { Label } from '~/components/label';
 import { RadioGroup, RadioGroupItem } from '~/components/radio-group';
+
+import { OptionsPartialType } from '../companion-recruitment-filter';
 
 interface optionProps {
   label?: string;
@@ -18,6 +20,8 @@ interface RadioItemProps {
 interface RadioFieldProps {
   category: string;
   options: optionProps[];
+  setOption: (category: string, selectedOption: OptionsPartialType) => void;
+  fieldName: string;
 }
 
 const RadioItem = ({ label, value, onClick }: RadioItemProps) => {
@@ -31,15 +35,24 @@ const RadioItem = ({ label, value, onClick }: RadioItemProps) => {
   );
 };
 
-const RadioField = ({ category, options }: RadioFieldProps) => {
+const RadioField = ({
+  category,
+  options,
+  setOption,
+  fieldName,
+}: RadioFieldProps) => {
   const defaultValue = options[0].value;
-  const [, setState] = useState('');
+  const [state, setState] = useState('');
 
   const onClick = (e: MouseEvent<HTMLButtonElement>) => {
     const { value } = e.currentTarget;
 
     setState(value);
   };
+
+  useEffect(() => {
+    setOption(fieldName, state);
+  }, [state, setOption, fieldName]);
 
   return (
     <div>
