4dac695e6678dfd5c1fe4f869eaee2bbb4d51312
yeonkyoung <keep.ykk@gmail.com>
2024-03-05 23:47:52 +0900

    Refactor: forwardRef삭제

--- app/_components/filter-field/select-field.tsx
+++ app/_components/filter-field/select-field.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { forwardRef, useState } from 'react';
+import { useState } from 'react';
 
 import {
   Select,
@@ -28,49 +28,38 @@ const SelectOptionItem = ({ value, label }: SelectOptionItemProps) => {
   return <SelectItem value={optionValue}>{label}</SelectItem>;
 };
 
-const SelectField = forwardRef<number, SelectFieldProps>(
-  ({ category, options, defaultValue, placeholder }, ref) => {
-    const defaultString = defaultValue.toString();
-    const [selectValue, setSelectValue] = useState(defaultString);
+const SelectField = ({
+  category,
+  options,
+  defaultValue,
+  placeholder,
+}: SelectFieldProps) => {
+  const defaultString = defaultValue.toString();
+  const [selectValue, setSelectValue] = useState(defaultString);
 
-    if (!ref) {
-      return;
-    }
-    if (typeof ref === 'function') {
-      return;
-    }
-    if (!ref.current) {
-      return;
-    }
-    ref.current = parseInt(selectValue);
+  parseInt(selectValue);
 
-    const onValueChange = (value: string) => {
-      setSelectValue(value);
-    };
+  const onValueChange = (value: string) => {
+    setSelectValue(value);
+  };
 
-    return (
-      <div>
-        <span className="font-semibold">{category}</span>
-        <Select onValueChange={onValueChange}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue
-              placeholder={placeholder}
-              defaultValue={defaultString}
-            />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              {options.map(({ label, value }) => (
-                <SelectOptionItem key={label} label={label} value={value} />
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-    );
-  },
-);
-
-SelectField.displayName = 'SelectField';
+  return (
+    <div>
+      <span className="font-semibold">{category}</span>
+      <Select onValueChange={onValueChange}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder={placeholder} defaultValue={defaultString} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {options.map(({ label, value }) => (
+              <SelectOptionItem key={label} label={label} value={value} />
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
 
 export default SelectField;
