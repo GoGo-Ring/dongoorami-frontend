669ed4a0a060c5112731b7f6f44c24f24bcb6e0c
junly21 <jaejoon0721@ajou.ac.kr>
2024-03-05 20:31:48 +0900

    Style: 캐러샐, 필터  레이아웃 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
-import { Button } from '~/components/button';
+import Filter from './_components/Filter';
 
 const Page = () => {
   return (
     <div>
-      <div className="flex flex-col gap-y-6 border border-black py-6 ">
-        <div className="flex h-[426px] justify-center bg-yellow-500">
-          <div className="h-full w-[900px] bg-yellow-300"></div>
+      <div className="flex aspect-carousel w-full flex-col gap-y-6 border border-black py-6 ">
+        <div className=" flex h-full justify-center">
+          <div className="w-[54px] bg-yellow-500"></div>
+          <div className=" w-[900px] bg-yellow-300"></div>
+          <div className="w-[54px] bg-yellow-500"></div>
         </div>
         <div className="h-[12px] bg-green-500"></div>
       </div>
-      <Button>test</Button>
+      <div className="flex w-full border border-black">
+        <div className="flex w-[250px] justify-center border p-8 ">
+          <Filter />
+        </div>
+        <div className="flex w-full justify-center border bg-red-200 p-8"></div>
+      </div>
     </div>
   );
 };
