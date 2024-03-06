5bd3ce590a826c0eddbe53e957a83af720449725
yeonkyoung <keep.ykk@gmail.com>
2024-03-06 10:48:49 +0900

    Refactor [#43] 동행 구함 Filter 컴포넌트 내부에 props 전달 (#55)
    * Chore: 컴포넌트 명, 파일명 변경
    
    * Refactor: 수정된 컴포넌트명으로 변경
    
    * Chore: 파일명 변경
    
    * Refactor: 인원수 카테고리의 options value를 string타입에서 number타입으로 수정
    
    * Refactor: 각 필드의 타입 분리
    
    * Feat: SelectField에 state 추가
    
    * Feat: RadioField에 state 추가
    
    * Refactor: 필터 외부에서 state를 변경지 않는 방식으로 변경하면서 onClick과 checked 제거
    
    * Refactor: SelectionField를 SelectRield로 변경
    
    * Feat: placeholder 추가
    
    * Feat: useState로 필터 항목에 대한 상태를 관리
    
    * Refactor: useState를 useRef로 변경
    
    * Refactor: ref의 초기값 설정은 SELECTION 객체를 reduce로 순회하여 생성
    
    * Feat: SelectField에 handleState추가
    
    * Feat: SELECTION 객체에 AGE항목 추가
    
    * Feat: InputItem에 value 추가
    
    * Refactor: InputField의 state를 contextAPI로 전달
    
    * Refactor: CompanionRecruitmentFilterContainer에서 내부 Field 컴포넌트를 다른 파일로 분리
    
    * Chore: 파일명 변경
    
    * Refactor: SelectField 컴포넌트 분리, context 삭제
    
    * Refactor: RadioField 컴포넌트 분리, context 삭제
    
    * Refactor: InputField 컴포넌트 분리, context 삭제
    
    * Refactor: state를 ref로 변경
    
    * Refactor: children을 RadioField로 변경
    
    * Chore: 파일 삭제
    
    * Chore: 동행구인 필터와 공연 필터의 constants 합치기
    
    * Feat: CheckboxSelectField 추가
    
    * Refactor: ref 추가
    
    * Refactor: checkbox ref 추가
    
    * Refactor: 타입 수정
    
    * Feat: 교통수단 ref 추가
    
    * Refactor: 교통수단 초기값 변경
    
    * Refactor: 입력 필드 값 변경시 ref 최신화
    
    * Feat: InputField 추가
    
    * Refactor: state를 ref로 수정
    
    * Feat: SelectField 추가
    
    * Chore: 파일 삭제
    
    * Refactor: 나이 options 변경
    
    * Refactor: 남자, 여자를 남,여로 수정
    
    * Refactor: checkbox의 값 변경 방식을 ref에서 state로 변경
    
    * Refactor: Input 값 변경 방식을 ref에서 state로 변경
    
    * Refactor: radioGroup의 값 변경 방식을 ref에서 state로 변경
    
    * Refactor: select 값 변경 방식을 ref에서 state로 변경
    
    * Refactor: age의 타입 수정
    
    * Refactor: acc 를 regions로 수정
    
    * Chore: 파일 삭제
    
    * Refactor: forwardRef 삭제, useEffect에서 setOption호출로 상태 업데이트
    
    * Refactor: forwardRef 삭제
    
    * Refactor: setOption, fieldName props추가
    
    * Feat: React.memo 적용
    
    * Feat: 상태 초기값 설정
    
    * Refactor: forwardRef 삭제
    
    * Refactor: useEffect에서 setOption 실행
    
    * Refactor: forwardRef삭제
    
    * Refactor: useEffect에서 setOption 실행
    
    * Refactor: React.memo 적용
    
    * Refactor: ref 삭제, state로 변경
    
    * Feat: useCallback을 적용한 getValue함수 추가
    
    setOptions 호출
    
    * Refactor: selectValue를 number로 변경

---
+++ app/_components/companion-recruitment-filter.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+
+import { Button } from '~/components/button';
+import { SEARCH, SELECTION } from '~/constants/filterField';
+
+import CheckboxSelectField from './filter-field/checkbox-select';
+import InputField from './filter-field/input-field';
+import RadioField from './filter-field/radio-field';
+import SelectField from './filter-field/select-field';
+
+export interface OptionsType {
+  gender: string;
+  region: string[];
+  age: [number, number];
+  transportation: string;
+  personCount: number;
+}
+
+export type OptionsPartialType = string | string[] | [number, number] | number;
+
+const CompanionRecruitmentFilter = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [options, setOptions] = useState({
+    gender: 'irrelevant',
+    region: [],
+    age: [20, 30],
+    transportation: '',
+    personCount: 0,
+  });
+
+  const handle = () => {};
+
+  const getValue = useCallback(
+    (category: string, selectedOption: OptionsPartialType) => {
+      setOptions(options => ({ ...options, [category]: selectedOption }));
+    },
+    [],
+  );
+
+  return (
+    <div className="flex w-[260px] flex-col gap-6 px-3">
+      <RadioField
+        category={SELECTION.GENDER.category}
+        options={SELECTION.GENDER.options}
+        setOption={getValue}
+        fieldName={'gender'}
+      />
+      <CheckboxSelectField
+        category={SELECTION.REGIONS.category}
+        options={SELECTION.REGIONS.options}
+        setOption={getValue}
+        fieldName={'region'}
+      />
+      <RadioField
+        category={SELECTION.TRANSPORTATION.category}
+        options={SELECTION.TRANSPORTATION.options}
+        setOption={getValue}
+        fieldName={'transportation'}
+      />
+      <InputField
+        category={SELECTION.AGE.category}
+        defaultValues={SELECTION.AGE.options}
+        setOption={getValue}
+        fieldName={'age'}
+      />
+      <SelectField
+        category={SELECTION.PERSON_COUNT.category}
+        options={SELECTION.PERSON_COUNT.options}
+        defaultValue={SELECTION.PERSON_COUNT.options[0].value}
+        placeholder={SELECTION.PERSON_COUNT.options[0].label}
+        setOption={getValue}
+        fieldName={'personCount'}
+      />
+      <Button variant="outline" onClick={handle}>
+        {SEARCH}
+      </Button>
+    </div>
+  );
+};
+
+export default CompanionRecruitmentFilter;
--- app/_components/filter-field/checkbox-select.tsx
+++ app/_components/filter-field/checkbox-select.tsx
@@ -1,23 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { MouseEvent, memo, useEffect, useState } from 'react';
 
 import { Checkbox } from '~/components/checkbox';
 
-interface SelectionFieldProps {
-  category: string;
-  options: string[];
-  isMultipleSelection?: boolean;
-}
+import { OptionsPartialType } from '../companion-recruitment-filter';
 
 interface ItemProps {
   label: string;
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+}
+interface CheckboxSelectFieldProps {
+  category: string;
+  options: string[];
+  setOption: (category: string, selectedOption: OptionsPartialType) => void;
+  fieldName: string;
 }
 
-const CheckboxItem = ({ label }: ItemProps) => {
+const CheckboxItem = ({ label, onClick }: ItemProps) => {
   return (
     <div className="flex items-center space-x-2">
-      <Checkbox id={label} />
+      <Checkbox id={label} value={label} onClick={onClick} />
       <label
         htmlFor={label}
         className="text-sm font-medium leading-none hover:cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -28,17 +31,45 @@ const CheckboxItem = ({ label }: ItemProps) => {
   );
 };
 
-const CheckboxSelectField = ({ category, options }: SelectionFieldProps) => {
+const CheckboxSelectField = ({
+  category,
+  options,
+  setOption,
+  fieldName,
+}: CheckboxSelectFieldProps) => {
+  const [checkbox, setCheckbox] = useState(
+    options.reduce(
+      (acc, option) => {
+        acc[option] = false;
+
+        return acc;
+      },
+      {} as Record<string, boolean>,
+    ),
+  );
+
+  useEffect(() => {
+    const selected = Object.keys(checkbox).filter(key => checkbox[key]);
+
+    setOption(fieldName, selected);
+  }, [checkbox, setOption, fieldName]);
+
+  const onClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const { value } = e.currentTarget;
+
+    setCheckbox({ ...checkbox, [value]: !checkbox[value] });
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <span className="font-semibold">{category}</span>
       <div className="flex flex-col gap-1">
         {options.map(option => (
-          <CheckboxItem key={option} label={option} />
+          <CheckboxItem key={option} label={option} onClick={onClick} />
         ))}
       </div>
     </div>
   );
 };
 
-export default CheckboxSelectField;
+export default memo(CheckboxSelectField);
