fa260906c42fe688889fdec250fd0b7857150c6e
junly21 <jaejoon0721@ajou.ac.kr>
2024-03-06 18:41:43 +0900

    Chore: BASE_URL변경

--- apis/index.ts
+++ apis/index.ts
@@ -5,7 +5,7 @@ import axios, {
   Method,
 } from 'axios';
 
-export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL = 'https://www.dongoorami.shop:8080/api/v1';
 
 export const instance = axios.create({
   // TODO BaseURL, Authorization 설정
