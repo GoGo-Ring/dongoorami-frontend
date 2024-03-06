c3c4178e89be4b635fa78117d48fb9fd4551ac0b
yeonkyoung <keep.ykk@gmail.com>
2024-03-07 00:52:23 +0900

    Refactor: InfoItem 컴포넌트 생성 및 변경

--- app/performance/page.tsx
+++ app/performance/page.tsx
@@ -1,6 +1,40 @@
 import Image from 'next/image';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/tabs';
+import { cn } from '~/libs/utils';
+
+interface InfoItemProps {
+  className?: string;
+  label: string;
+  contents: string[] | string;
+  labelClassName?: string;
+  contentClassName?: string;
+}
+
+const InfoItem = ({
+  className,
+  label,
+  contents,
+  labelClassName,
+  contentClassName,
+}: InfoItemProps) => {
+  const isArray = typeof contents !== 'string';
+
+  return (
+    <div className={cn('flex', className)}>
+      <span className={cn('font-semibold', labelClassName)}>{label}</span>
+      <div className={cn('flex', contentClassName)}>
+        {isArray ? (
+          contents.map((text, index) => (
+            <span key={`${text}_${index}`}>{text}</span>
+          ))
+        ) : (
+          <span>{contents}</span>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const Page = () => {
   return (
@@ -9,83 +43,110 @@ const Page = () => {
       <div className="flex items-center gap-14 sm:flex-col">
         <Image src={''} width={300} height={400} alt={`${'공연명'} 포스터`} />
         <div className="flex w-full flex-col gap-4 px-3 pt-3">
-          <div className="flex sm:flex-col">
-            <span className="w-[90px] font-semibold">장소</span>
-            <span>장소명 (장소명) (장소명)</span>
-          </div>
-          <div className="flex sm:flex-col">
-            <span className="w-[90px] font-semibold">공연기간</span>
-            <span>2024.01.01&nbsp;~&nbsp;2024.01.01</span>
-          </div>
-          <div className="flex sm:flex-col">
-            <span className="w-[90px] font-semibold">공연시간</span>
-            <span>90분</span>
-          </div>
-          <div className="flex">
-            <span className="w-[90px] font-semibold">가격</span>
-            <div className="flex flex-col">
-              <span>30,000원</span>
-              <span>30,000원</span>
-              <span>30,000원</span>
-              <span>30,000원</span>
-              <span>30,000원</span>
-            </div>
-          </div>
+          <InfoItem
+            label={'장소'}
+            className="sm:flex-col"
+            contents={'장소명 (장소명) (장소명)'}
+            labelClassName="w-[90px]"
+          />
+          <InfoItem
+            label={'공연기간'}
+            className="sm:flex-col"
+            contents={'2024.01.01 ~ 2024.01.01'}
+            labelClassName="w-[90px]"
+          />
+          <InfoItem
+            label={'공연시간'}
+            className="sm:flex-col"
+            contents={'90분'}
+            labelClassName="w-[90px]"
+          />
+          <InfoItem
+            label={'가격'}
+            contents={[
+              '30,000원',
+              '30,000원',
+              '30,000원',
+              '30,000원',
+              '30,000원',
+            ]}
+            labelClassName="w-[90px]"
+            contentClassName="flex-col"
+          />
         </div>
       </div>
       <div>
         <Tabs defaultValue="information">
           <TabsList className="grid w-full grid-cols-3">
             <TabsTrigger value="information">공연 정보</TabsTrigger>
-            <TabsTrigger value="recruitment">공연 구인 (0)</TabsTrigger>
-            <TabsTrigger value="review">관람 후기 (0)</TabsTrigger>
+            <TabsTrigger value="recruitment">공연 구인 ({0})</TabsTrigger>
+            <TabsTrigger value="review">관람 후기 ({0})</TabsTrigger>
           </TabsList>
           <TabsContent
             value="information"
             className="flex w-full flex-col gap-6 p-6"
           >
-            <div className="flex flex-col gap-4 sm:gap-1">
-              <span className="text-xl font-semibold">출연진</span>
-              <div>
-                <span>출연진1</span>
-                <span>출연진2</span>
-                <span>출연진3</span>
-              </div>
-            </div>
-            <div className="flex flex-col gap-4 sm:gap-1">
-              <span className="text-xl font-semibold">줄거리</span>
-              <div>
-                <p>줄거리 설명</p>
-              </div>
-            </div>
+            <InfoItem
+              label={'출연진'}
+              className="flex-col gap-4 sm:gap-1"
+              contents={['출연진1', '출연진2', '출연진3']}
+              labelClassName="text-xl font-semibold"
+              contentClassName="gap-1"
+            />
+            <InfoItem
+              label={'줄거리'}
+              className="flex-col gap-4 sm:gap-1"
+              contents={'줄거리 설명'}
+              labelClassName="text-xl font-semibold"
+            />
+
             <div className="grid grid-cols-2 gap-6">
-              <div className="flex flex-col gap-4 sm:gap-1">
-                <span className="text-xl font-semibold">제작진</span>
-                <span>제작진</span>
-              </div>
-              <div className="flex flex-col gap-4 sm:gap-1">
-                <span className="text-xl font-semibold">기획사</span>
-                <span>기획사</span>
-              </div>
-              <div className="flex flex-col gap-4 sm:gap-1">
-                <span className="text-xl font-semibold">주최</span>
-                <span>주최</span>
-              </div>
-              <div className="flex flex-col gap-4 sm:gap-1">
-                <span className="text-xl font-semibold">주관</span>
-                <span>주관</span>
-              </div>
+              <InfoItem
+                label={'제작진'}
+                className="flex-col gap-4 sm:gap-1"
+                contents={['제작진', '제작진', '제작진']}
+                labelClassName="text-xl font-semibold"
+                contentClassName="gap-1"
+              />
+              <InfoItem
+                label={'기획사'}
+                className="flex-col gap-4 sm:gap-1"
+                contents={['기획사', '기획사', '기획사']}
+                labelClassName="text-xl font-semibold"
+                contentClassName="gap-1"
+              />
+              <InfoItem
+                label={'주최'}
+                className="flex-col gap-4 sm:gap-1"
+                contents={'주최'}
+                labelClassName="text-xl font-semibold"
+                contentClassName="gap-1"
+              />
+              <InfoItem
+                label={'주관'}
+                className="flex-col gap-4 sm:gap-1"
+                contents={'주관'}
+                labelClassName="text-xl font-semibold"
+                contentClassName="gap-1"
+              />
             </div>
-            <div className="flex flex-col gap-4 sm:gap-1">
-              <span className="text-xl font-semibold">공연 시간</span>
-              <span>화요일 ~ 금요일 (20:00)</span>
-              <span>토요일 (20:00)</span>
-              <span>화요일 ~ 금요일 (10:00)</span>
-            </div>
-            <div className="flex flex-col gap-4 sm:gap-1">
-              <span className="text-xl font-semibold">공연 관람 연령</span>
-              <span>만 12세 이상</span>
-            </div>
+            <InfoItem
+              label={'공연 시간'}
+              className="flex-col gap-4 sm:gap-1"
+              contents={[
+                '화요일 ~ 금요일 (20:00)',
+                '토요일 (20:00)',
+                '화요일 ~ 금요일 (10:00)',
+              ]}
+              labelClassName="text-xl font-semibold"
+              contentClassName="flex-col"
+            />
+            <InfoItem
+              label={'공연 관람 연령'}
+              className="flex-col gap-4 sm:gap-1"
+              contents={'만 12세 이상'}
+              labelClassName="text-xl font-semibold"
+            />
             <div className="flex flex-col gap-4 sm:gap-1">
               <span className="text-xl font-semibold">공연 상세</span>
               <Image
@@ -97,10 +158,10 @@ const Page = () => {
             </div>
           </TabsContent>
           <TabsContent value="recruitment">
-            <span>hi?</span>
+            <span>공연 구인</span>
           </TabsContent>
           <TabsContent value="review">
-            <span>hi?</span>
+            <span>관람 후기</span>
           </TabsContent>
         </Tabs>
       </div>
