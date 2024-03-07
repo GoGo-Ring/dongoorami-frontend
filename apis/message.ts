99fc4f2fcb042cd5df556ce96e8046da0a4ee31a
ienrum <azang0506@gmail.com>
2024-03-07 22:08:56 +0900

    Feat: message create, getById msw api 작성

--- apis/message.ts
+++ apis/message.ts
@@ -14,3 +14,33 @@ export const getMessages = async (size: number, page: number) => {
 export const updateMessage = async (id: number) => {
   await api.patch({ url: `/messages/${id}` });
 };
+
+export const createMessage = async ({
+  senderId,
+  receiverId,
+  content,
+  accompanyPostId,
+}: {
+  senderId: number;
+  receiverId: number;
+  content: string;
+  accompanyPostId?: number;
+}) => {
+  await api.post({
+    url: '/messages',
+    data: { senderId, receiverId, content, accompanyPostId },
+  });
+};
+
+export const getMessagesById = async (
+  id: number,
+  size: number,
+  page: number,
+) => {
+  const { data } = await api.get<MessageWithPage>({
+    url: `/messages/${id}`,
+    params: { size, page },
+  });
+
+  return data;
+};
