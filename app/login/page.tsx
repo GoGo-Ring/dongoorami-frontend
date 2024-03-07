5ddb96771867c4b2110724fffd1ca15ee1c48ec5
junly21 <jaejoon0721@ajou.ac.kr>
2024-03-07 10:28:43 +0900

    Chore: 오타 주석 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ const Login = () => {
     <div className="flex h-screen w-full items-center justify-center">
       <div className="flex-column h-96 w-96 min-w-96 space-y-8 p-1 ">
         <h1 className="text-shadow text-center font-title text-6xl text-primary">
-          동구라미{' '}
+          동구라미
         </h1>
         <div className="rounded-md border border-slate-200 p-8">
           <OAuthButton domain="kakao" />
