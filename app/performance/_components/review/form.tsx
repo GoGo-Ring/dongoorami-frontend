74d5d107706de84ce5a92b8f3dadebf0db8f1b88
ienrum <azang0506@gmail.com>
2024-03-08 11:07:50 +0900

    Refactor: validation 대신 ux 적으로 문제 해결
    initialValue 를 넣어주거나, 빈값이 반환되도록 함으로 써 해결

--- app/performance/_components/review/form.tsx
+++ app/performance/_components/review/form.tsx
@@ -4,18 +4,22 @@ import { useState } from 'react';
 
 import StarRating from '~/app/review/_components/star-rating';
 import { Button } from '~/components/button';
-import ErrorText from '~/components/error-text';
 import { Input } from '~/components/input';
 import { Textarea } from '~/components/textarea';
 import useForm from '~/hooks/useForm';
 
-const ReviewForm = () => {
+interface ReviewFormProps {
+  initialTitle: string;
+  intialContent?: string;
+}
+
+const ReviewForm = ({ initialTitle, intialContent }: ReviewFormProps) => {
   const [rate, setRate] = useState(0);
-  const { handleUnContolledSubmit, errors } = useForm({
+  const { handleUnContolledSubmit } = useForm({
     initialValues: {
       star: '0',
-      title: '',
-      content: '',
+      title: initialTitle,
+      content: intialContent || '',
     },
     onSubmit: values => {
       // TODO: submit review
@@ -23,42 +27,24 @@ const ReviewForm = () => {
 
       return values;
     },
-    validationRulesList: [
-      {
-        id: 'star',
-        validate: value => Number(value) > 0,
-        message: '별점을 선택해주세요.',
-      },
-      {
-        id: 'title',
-        validate: value => value.length <= 30 && value.length >= 1,
-        message: '리뷰 제목은 1자 이상 30자 이하로 입력해주세요.',
-      },
-      {
-        id: 'content',
-        validate: value => value.length <= 100 && value.length >= 1,
-        message: '리뷰는 1자 이상 100자 이하로 입력해주세요.',
-      },
-    ],
   });
 
   return (
-    <form onSubmit={handleUnContolledSubmit} className="flex flex-col">
+    <form onSubmit={handleUnContolledSubmit} className="flex flex-col gap-4">
       <StarRating rate={rate} setRate={setRate} id="star" />
-      <ErrorText message={errors.star} />
       <Input
         className="w-1/2"
-        placeholder="리뷰 제목을 입력해주세요."
+        placeholder={initialTitle}
+        defaultValue={initialTitle}
         id="title"
       />
-      <ErrorText message={errors.title} />
       <Textarea
         name="content"
         id="content"
         className="resize-none"
         placeholder="리뷰를 입력해주세요."
+        defaultValue={intialContent}
       />
-      <ErrorText message={errors.content} />
       <Button type="submit" className="self-end" disabled={false}>
         입력
       </Button>
