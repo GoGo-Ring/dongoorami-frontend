2a4c9404efd8c353927f612786805496a15bbdfd
yeonkyoung <keep.ykk@gmail.com>
2024-03-08 17:17:52 +0900

    Feat: absolute 적용하면서 중복 코드 제거

--- app/performance/_components/performance-review.tsx
+++ app/performance/_components/performance-review.tsx
@@ -15,19 +15,17 @@ export const PerformanceReview = ({
   starRating,
 }: PerformanceReviewProps) => {
   return (
-    <div className="flex flex-col gap-1 sm:w-[250px] md:w-[630px] lg:w-[840px]">
-      <div className="sm: md: flex justify-between">
+    <div className="relative flex flex-col gap-1 sm:w-[250px] md:w-[630px] lg:w-[840px]">
+      <div className="flex justify-between">
         <StarRating rate={starRating} />
-        <div className="flex gap-3 divide-x text-gray-300">
+        <div className="flex gap-3 divide-x text-gray-300 sm:absolute sm:bottom-0 sm:right-0">
           <span>{userId}</span>
           <span className="pl-3 sm:hidden">{date}</span>
         </div>
       </div>
       <h3 className="text-base font-semibold">{title}</h3>
       <p>{content}</p>
-      <span className="text-right text-gray-300 md:hidden lg:hidden">
-        {date}
-      </span>
+      <span className="md:hidden lg:hidden">&nbsp;</span>
     </div>
   );
 };
