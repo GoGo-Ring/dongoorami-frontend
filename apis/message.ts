533cfacef36df0ef786ff32c4d8d3628646b32d3
ienrum <azang0506@gmail.com>
2024-03-09 13:52:10 +0900

    Refactor: createMessage 타입 분리

--- apis/message.ts
+++ apis/message.ts
@@ -15,17 +15,19 @@ export const updateMessage = async (id: number) => {
   await api.patch({ url: `/messages/${id}` });
 };
 
+interface CreateMessageParams {
+  senderId: number;
+  receiverId: number;
+  content: string;
+  accompanyPostId?: number;
+}
+
 export const createMessage = async ({
   senderId,
   receiverId,
   content,
   accompanyPostId,
-}: {
-  senderId: number;
-  receiverId: number;
-  content: string;
-  accompanyPostId?: number;
-}) => {
+}: CreateMessageParams) => {
   await api.post({
     url: '/messages',
     data: { senderId, receiverId, content, accompanyPostId },
