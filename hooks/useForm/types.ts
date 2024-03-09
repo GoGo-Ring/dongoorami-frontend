e1517ffbcac495ba2b4e64bfd5bcfb27636aabac
ienrum <137467530+ienrum@users.noreply.github.com>
2024-03-09 09:41:18 +0900

    Refactor: [#101] useForm 리팩토링 (#102)
    * Refactor: useForm 리팩토링 및 uncontrolled handleSubmit 함수 추가
    
    * Refactor: 구인 입력 페이지에 새로운 useForm 적용
    
    * Refactor: 구인 상세페이지의 댓글컴포넌트에, useForm 적용

--- hooks/useForm/types.ts
+++ hooks/useForm/types.ts
@@ -1,61 +1,51 @@
+type InputType = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
+
 type HandleChange = <
   E extends HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement,
 >(
   e: React.ChangeEvent<E>,
 ) => void;
 type HandleValueChange<T> = (id: keyof T) => (value: string) => void;
-type handleChangeField<T> = (newValues: Partial<T>) => void;
-type HandleSliderInputChange<T> = (props: {
-  id: keyof T;
-  minId: keyof T;
-  maxId: keyof T;
-}) => (e: React.ChangeEvent<HTMLInputElement>) => void;
-type HandleSliderValueChange<T> = (props: {
-  id: keyof T;
-  minId: keyof T;
-  maxId: keyof T;
-}) => (value: number[]) => void;
 
 type HandleSubmit = (e: React.FormEvent<HTMLFormElement>) => void;
 
 type Errors<T> = Record<keyof T, string>;
 
 type ValidateFn = (value: string, values: Record<string, string>) => boolean;
-type ValidateForm<T> = () => Errors<T>;
-type UpdateField<T> = (newValues: Record<keyof T, string>) => void;
+type ValidateForm<T extends Record<string, string>> = (values: T) => Errors<T>;
+type ValidateField<T> = (id: keyof T, value: string, values: T) => string;
 
+type UpdateField<T> = (id: keyof T, value: string) => void;
+
+type ValidationRules<T> = Record<keyof T, Record<string, ValidateFn>>;
 type RegisterValidation<T> = (props: {
   id: keyof T;
   validate: ValidateFn;
   message: string;
 }) => void;
 
-type ValidationRules<T> = Record<keyof T, Record<string, ValidateFn>>;
-
 type UseFormReturn<T> = {
   values: T;
   errors: Errors<T>;
   setValues: React.Dispatch<React.SetStateAction<T>>;
   handleChange: HandleChange;
-  handleChangeField: handleChangeField<T>;
-  handleSliderInputChange: HandleSliderInputChange<T>;
   handleValueChange: HandleValueChange<T>;
+  handleUnContolledSubmit: HandleSubmit;
   handleSubmit: HandleSubmit;
-  handleSliderValueChange: HandleSliderValueChange<T>;
   registerValidation: RegisterValidation<T>;
 };
 
 export type {
   HandleChange,
   HandleValueChange,
-  HandleSliderInputChange,
-  HandleSliderValueChange,
   HandleSubmit,
   Errors,
   ValidateFn,
+  ValidateField,
   ValidateForm,
   UpdateField,
   RegisterValidation,
   ValidationRules,
   UseFormReturn,
+  InputType,
 };
