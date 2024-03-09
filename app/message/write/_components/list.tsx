1de4fd24bd75458e391b09429a74d66666ef3aae
ienrum <azang0506@gmail.com>
2024-03-09 13:53:32 +0900

    Refactor: 쪽지 ui 추가
    내 쪽지가 무엇인지 더잘띌수있도록
    
    border to divide-y

--- app/message/write/_components/list.tsx
+++ app/message/write/_components/list.tsx
@@ -1,4 +1,5 @@
 import useFetchMessageById from '~/hooks/queries/useFetchMessageById';
+import { cn } from '~/libs/utils';
 import { getDateWithTime } from '~/utils/dateFormatter';
 
 interface MessageWriteListProps {
@@ -7,11 +8,22 @@ interface MessageWriteListProps {
 
 const MessageWriteList = ({ targetId }: MessageWriteListProps) => {
   const { data } = useFetchMessageById(targetId, 100, 1);
+  const myId = 2; // TODO: 유저 로직 추가
+  const isMyId = (id: number) => id === myId;
 
   return (
-    <div>
+    <div className="divide-y">
       {data?.messages?.map(({ id, content, date, senderId }) => (
-        <div key={id} className="flex flex-col border-b-2 p-2">
+        <div
+          key={id}
+          className={cn(
+            'text-bold flex flex-col p-2 text-sm ',
+            isMyId(senderId) ? ' bg-gray-150 ' : '',
+          )}
+        >
+          <p className="text-gray-700">
+            {`유저 ${senderId} ${isMyId(senderId) ? '(나)' : ''}`}
+          </p>
           <p className="line-clamp-4 whitespace-pre-wrap text-wrap text-gray-600 hover:block hover:overflow-visible">
             {content}
           </p>
@@ -19,7 +31,6 @@ const MessageWriteList = ({ targetId }: MessageWriteListProps) => {
             <p className="text-gray-300">
               {getDateWithTime(new Date(date), 'yyyy.mm.dd', 'hh:mm:ss')}
             </p>
-            <p className="text-gray-700">유저 {senderId}</p>
           </div>
         </div>
       ))}
