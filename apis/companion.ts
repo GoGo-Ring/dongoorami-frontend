58c95c6a0f4f334777c00db233b2ee845b1257f7
yeonkyoung <keep.ykk@gmail.com>
2024-03-11 20:15:44 +0900

    Refactor: getCompanions로 수정

--- apis/companion.ts
+++ apis/companion.ts
@@ -2,7 +2,7 @@ import { CompanionRecruitmentCard } from './scheme/accompany';
 
 import api from '.';
 
-const getCompanion = async (): Promise<CompanionRecruitmentCard[]> => {
+const getCompanions = async (): Promise<CompanionRecruitmentCard[]> => {
   const { data } = await api.get<CompanionRecruitmentCard[]>({
     url: '/search/posts',
   });
@@ -10,4 +10,4 @@ const getCompanion = async (): Promise<CompanionRecruitmentCard[]> => {
   return data;
 };
 
-export default getCompanion;
+export default getCompanions;
