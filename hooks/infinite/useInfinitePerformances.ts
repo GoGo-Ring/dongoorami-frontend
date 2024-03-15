e64468fad7988963df49a3a4d0accdc476c5319a
yeonkyoung <keep.ykk@gmail.com>
2024-03-16 00:45:04 +0900

    Refactor: 변경된 함수명으로 수정

--- hooks/infinite/useInfinitePerformances.ts
+++ hooks/infinite/useInfinitePerformances.ts
@@ -1,13 +1,13 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 
-import getPerformances from '~/apis/performance';
+import { getPerformancesList } from '~/apis/performance';
 import { PerformanceList } from '~/apis/scheme/performance';
 
 export const useInfinitePerformances = (params: string) => {
   return useInfiniteQuery({
     queryKey: ['performances', params],
     queryFn: async ({ pageParam }): Promise<PerformanceList> => {
-      return getPerformances(params, pageParam);
+      return getPerformancesList(params, pageParam);
     },
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPage) => {
