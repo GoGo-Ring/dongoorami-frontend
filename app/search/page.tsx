2c581da64b51d14c01fcdc8d323633a7127fbc79
yeonkyoung <keep.ykk@gmail.com>
2024-03-15 13:17:54 +0900

    Feat: 버튼 클릭으로 무한스크롤 시작하는 기능 추가

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,16 +1,39 @@
 'use client';
 
-import { Button } from '~/components/button';
-import useFetchCompanions from '~/hooks/queries/useFetchCompanions';
-import useFetchPerformances from '~/hooks/queries/useFetchPerformances';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
 
-import PerformanceInfoCard from './_component/performance-info-card';
-import CompanionRecruitmentCard from '../_components/CompanionRecruitmentCard';
+import api from '~/apis';
+import { AccompanyPostInfoList } from '~/apis/scheme/accompany';
+import { Button } from '~/components/button';
+
+import useIntersectionObsever from './_component/useIntersectionObserver';
 import FilterTabs from '../_components/filter-tabs';
 
 const Page = () => {
-  const performancesQuery = useFetchPerformances();
-  const companionsQuery = useFetchCompanions();
+  const searchParams = useSearchParams();
+  const params = searchParams.toString();
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['accompanies', params],
+      queryFn: async ({ pageParam }): Promise<AccompanyPostInfoList> => {
+        const { data } = await api.get<AccompanyPostInfoList>({
+          url: `/accompanies/posts?${params}$_size=${pageParam}`,
+        });
+
+        return data;
+      },
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPage) => {
+        return lastPage.hasNext ? allPage.length + 1 : undefined;
+      },
+    });
+  const handleFetchNextPage = () => {
+    fetchNextPage();
+  };
+
+  useIntersectionObsever({ handleFetchNextPage, hasNextPage });
 
   return (
     <div className="flex">
@@ -18,34 +41,49 @@ const Page = () => {
         <FilterTabs />
       </div>
       <div className="flex flex-col gap-8 px-6 py-10">
-        <div className="flex flex-col gap-6">
-          <h3 className="font-semibold">공연</h3>
-          <div className="mx-auto grid grid-cols-3">
-            {performancesQuery?.data?.map(({ id, startDate, ...rest }) => (
-              <PerformanceInfoCard
-                key={id}
-                startDate={new Date(startDate)}
-                {...rest}
-              />
-            ))}
-          </div>
+        <Button
+          variant="outline"
+          disabled={!hasNextPage || isFetchingNextPage}
+          onClick={() => fetchNextPage()}
+        >
+          공연 더보기
+        </Button>
+        <div className="mx-auto grid grid-cols-3">
+          {data?.pages?.map(page =>
+            page.accompanyPostInfos.map(
+              ({
+                id,
+                title,
+                writer,
+                createdAt,
+                updatedAt,
+                status,
+                concertName,
+                viewCount,
+                commentCount,
+                gender,
+                totalPeople,
+              }) => (
+                <div className="h-96 w-40" key={id}>
+                  <span>{id}</span>
+                  <span>{title}</span>
+                  <span>{writer}</span>
+                  <span>{createdAt}</span>
+                  <span>{updatedAt}</span>
+                  <span>{status}</span>
+                  <span>{concertName}</span>
+                  <span>{viewCount}</span>
+                  <span>{commentCount}</span>
+                  <span>{gender}</span>
+                  <span>{totalPeople}</span>
+                </div>
+              ),
+            ),
+          )}
         </div>
-        <Button variant="outline">공연 더보기</Button>
-        <div>
-          <div className="flex flex-col gap-6">
-            <span className="font-semibold">동행 모집</span>
-            <div className="mx-auto grid grid-cols-3 gap-4">
-              {companionsQuery?.data?.map(({ id, date, ...rest }) => (
-                <CompanionRecruitmentCard
-                  key={id}
-                  date={new Date(date)}
-                  {...rest}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-        <Button variant="outline">동행 모집 더보기</Button>
+        <Button variant="outline" onClick={handleFetchNextPage}>
+          동행 모집 더보기
+        </Button>
       </div>
     </div>
   );
