2031852e519756abf47b71cb61fcc6b096eac6b7
yeonkyoung <keep.ykk@gmail.com>
2024-03-19 00:30:49 +0900

    Feat: SIZE 상수 추가

--- hooks/infinite/useInfinitePerformances.ts
+++ hooks/infinite/useInfinitePerformances.ts
@@ -3,11 +3,13 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { getPerformancesList } from '~/apis/performance';
 import { PerformanceList } from '~/apis/scheme/performance';
 
+const SIZE = 6;
+
 export const useInfinitePerformances = (params: string) => {
   return useInfiniteQuery({
     queryKey: ['performances', params],
     queryFn: async ({ pageParam }): Promise<PerformanceList> => {
-      return getPerformancesList(params, pageParam);
+      return getPerformancesList(params, SIZE, pageParam);
     },
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPage) => {
