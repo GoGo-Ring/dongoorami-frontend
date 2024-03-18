3d58823caff7f79b6cb2d54d2c8340c42f5ca456
yeonkyoung <keep.ykk@gmail.com>
2024-03-19 04:02:34 +0900

    Feat: 공연 상세 페이지 타입

--- apis/scheme/performance.ts
+++ apis/scheme/performance.ts
@@ -6,3 +6,28 @@ export interface PerformanceInfoCard {
   startDate: Date;
   status: '공연 예정' | '공연 중' | '공연 종료';
 }
+
+export interface ConcertDetail {
+  id: number;
+  name: string;
+  startedAt: string;
+  endedAt: string;
+  place: string;
+  actor: string;
+  crew: string;
+  runtime: string;
+  age: string;
+  producer: string;
+  agency: string;
+  host: string;
+  management: string;
+  cost: string;
+  poster: string;
+  summary: string;
+  genre: string;
+  status: string;
+  introductionImages: string[];
+  schedule: string;
+  totalAccompanies: number;
+  totalReviews: number;
+}
