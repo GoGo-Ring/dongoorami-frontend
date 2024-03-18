19aacbbf3e8d54409b5669caa632d841ae72caba
yeonkyoung <keep.ykk@gmail.com>
2024-03-18 11:54:32 +0900

    Refactor: performance-info-card 타입 분리, 날짜를 변경하는 getDate 삭제

--- app/search/_component/performance-info-card.tsx
+++ app/search/_component/performance-info-card.tsx
@@ -1,44 +1,42 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
+import { BASE_URL } from '~/apis';
+import { PerformanceInfoCard } from '~/apis/scheme/performance';
 import { Badge } from '~/components/badge';
-import { getDate } from '~/utils/dateFormatter';
-
-interface PerformanceInfoCardProps {
-  posterSrc: string;
-  title: string;
-  facilityName: string;
-  startDate: Date;
-  status: '공연 예정' | '공연 중' | '공연 종료';
-}
 
 const PerformanceInfoCard = ({
-  posterSrc,
-  title,
-  facilityName,
-  startDate,
+  id,
+  name,
+  place,
+  startedAt,
+  poster,
   status,
-}: PerformanceInfoCardProps) => {
+}: PerformanceInfoCard) => {
   const width = 224;
   const height = 288;
-  const formattedStartDate = getDate(startDate, 'yyyy-mm-dd');
 
   return (
     <div className="mr-5 w-56 cursor-pointer flex-col">
-      <Image
-        className="h-72 w-56 rounded-lg"
-        width={width}
-        height={height}
-        src={posterSrc}
-        alt={`${title} 포스터`}
-      />
-      <div className="flex flex-col px-1">
-        <h3 className="text-clip break-all text-base font-semibold">{title}</h3>
-        <span className="truncate text-sm">{facilityName}</span>
-        <span className="text-xs text-gray-300">{formattedStartDate}</span>
-        <Badge className="mt-2 w-fit rounded-md" variant="outline">
-          {status}
-        </Badge>
-      </div>
+      <Link href={`${BASE_URL}/performance/${id}`}>
+        <Image
+          className="h-72 w-56 rounded-lg"
+          width={width}
+          height={height}
+          src={poster}
+          alt={`${name} 포스터`}
+        />
+        <div className="flex flex-col px-1">
+          <h3 className="text-clip break-all text-base font-semibold">
+            {name}
+          </h3>
+          <span className="truncate text-sm">{place}</span>
+          <span className="text-xs text-gray-300">{startedAt}</span>
+          <Badge className="mt-2 w-fit rounded-md" variant="outline">
+            {status}
+          </Badge>
+        </div>
+      </Link>
     </div>
   );
 };
