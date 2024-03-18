418b62b311c1e4432febf9ba4d4623f19401acab
yeonkyoung <keep.ykk@gmail.com>
2024-03-19 01:40:20 +0900

    Feat: cursorId를 지정하기 위한 변수 lastId 추가

--- hooks/infinite/useInfiniteAccompanies.ts
+++ hooks/infinite/useInfiniteAccompanies.ts
@@ -3,15 +3,21 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { getCompanionsList } from '~/apis/companion';
 import { AccompanyPostInfoList } from '~/apis/scheme/accompany';
 
+const SIZE = 6;
 const useInfiniteAccompanies = (params: string) => {
   return useInfiniteQuery({
     queryKey: ['accompanies', params],
     queryFn: async ({ pageParam }): Promise<AccompanyPostInfoList> => {
-      return getCompanionsList(params, pageParam);
+      return getCompanionsList(params, SIZE, pageParam);
     },
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPage) => {
-      return lastPage.hasNext ? allPage.length + 1 : undefined;
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      const { accompanyPostInfos } = lastPage;
+      const { length } = accompanyPostInfos;
+
+      const lastId = length ? accompanyPostInfos[length - 1].id : undefined;
+
+      return lastPage.hasNext ? lastId : undefined;
     },
   });
 };
