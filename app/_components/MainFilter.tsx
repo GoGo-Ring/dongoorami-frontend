2ee435caa3026a4784d52b5d4d862774efee7dee
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-03-18 16:52:23 +0900

    Style: 필터가 좌측에 고정되도록 sticky속성 추가

--- app/_components/MainFilter.tsx
+++ app/_components/MainFilter.tsx
@@ -2,12 +2,14 @@ import CompanionRecruitmentFilter from './companion-recruitment-filter';
 
 const MainFilter = () => {
   return (
-    <div className="flex w-[220px] justify-center border p-8 sm:hidden">
-      <CompanionRecruitmentFilter
-        onSubmit={() => {
-          return 1;
-        }}
-      />
+    <div className="border  sm:hidden">
+      <div className="sticky top-0 flex h-[900px] w-[220px] justify-center p-4">
+        <CompanionRecruitmentFilter
+          onSubmit={() => {
+            return 1;
+          }}
+        />
+      </div>
     </div>
   );
 };
