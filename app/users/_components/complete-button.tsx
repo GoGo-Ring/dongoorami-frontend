01141c78e61f261c6c811635e45b7091cbeb9beb
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2024-03-19 10:59:04 +0900

    Feat: [#122] 내 정보, 다른 유저 정보 페이지 분리 (#123)
    * Refactor: 내 정보와 다른 유저 정보 분리
    
    * Feat: profile fetch api 생성
    
    * Refactor: 내 정보 라우터 변경
    
    * Test: 유저 정보 api mocking
    
    * Refactor: 유저 Image 중앙 정렬
    
    * Refactor: 유저 페이지 모바일 뷰 추가
    
    * Refactor: 모바일 메뉴 아이콘 변경
    
    * Refactor: 모바일 환경에서 햄버거 아이콘 사용

--- app/users/_components/complete-button.tsx
+++ app/users/_components/complete-button.tsx
@@ -14,7 +14,12 @@ const CompleteButton = <T,>({
   onComplete,
 }: CompleteButtonProps<T>) => {
   if (isEdit) {
-    return <Button onClick={() => onComplete(inputs)}>완료</Button>;
+    return (
+      <div className="flex items-center gap-3">
+        <Button onClick={handleIsEdit}>취소</Button>
+        <Button onClick={() => onComplete(inputs)}>완료</Button>
+      </div>
+    );
   }
 
   return <Button onClick={handleIsEdit}>수정</Button>;
