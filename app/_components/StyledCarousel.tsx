8a62534d26d0c786edb17a1d3878a122316cc7fa
junly21 <jaejoon0721@ajou.ac.kr>
2024-03-20 23:36:27 +0900

    Feat: 캐러샐 클릭 시 해당공연으로 이동

--- app/_components/StyledCarousel.tsx
+++ app/_components/StyledCarousel.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { PerformanceInfoListItemApi } from '~/apis/scheme/performance';
 import {
@@ -14,21 +15,21 @@ interface StyledCarouselProps {
 }
 
 const StyledCarousel = ({ performances }: StyledCarouselProps) => {
-  const images = performances.map(performance => performance.poster);
-
   return (
     <Carousel className="flex w-full justify-center border py-8 sm:aspect-square">
       <CarouselContent className="flex">
-        {images.map((image, index) => (
+        {performances.map((performance, index) => (
           <CarouselItem className=" flex justify-center " key={index}>
             <div className="flex w-[350px] justify-center border p-2">
-              <Image
-                width={500}
-                height={288}
-                className="h-full w-full "
-                src={image}
-                alt={`${image} 포스터`}
-              />
+              <Link href={`/performance/${performance.id}`}>
+                <Image
+                  width={500}
+                  height={288}
+                  className="h-full w-full "
+                  src={performance.poster}
+                  alt={`${performance.name}} 포스터`}
+                />
+              </Link>
             </div>
           </CarouselItem>
         ))}
