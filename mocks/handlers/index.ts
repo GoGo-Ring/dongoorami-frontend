458267c137f36cccec04312f655b5843c27e3794
ienrum <137467530+ienrum@users.noreply.github.com>
2024-03-20 16:52:09 +0900

    Feat: [#149] 동행 입력 페이지에 공연 목록 search 입력을 위한 msw와 api 추가 (#150)
    * Feat: api scheme 및 api 함수 추가
    
    * Feat: msw handler 추가
    
    * Feat: Fetch 훅 추가
    
    * Feat: fetch 훅 컴포넌트에 적용

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,5 +1,6 @@
 import accompany from './accompany';
 import accompanyDetail from './accompanyDetail';
+import accompanyPerformances from './accompanyPerformances';
 import comment from './comment';
 import members from './members';
 import message from './message';
@@ -12,5 +13,6 @@ export const handlers = [
   ...message,
   ...comment,
   ...accompanyDetail,
+  ...accompanyPerformances,
   ...reviews,
 ];
