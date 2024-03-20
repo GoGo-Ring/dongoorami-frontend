150d5b0fe227740a4ae654978620e2cd8eb8198a
junly21 <jaejoon0721@ajou.ac.kr>
2024-03-20 10:22:55 +0900

    Feat: 무한스크롤 기능 완성. 리팩토링 필요

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,46 @@
 'use client';
 
-import { DummyCard } from './_components/DummyCard';
+import { useState } from 'react';
+
+import CompanionRecruitmentList from '~/app/search/_component/companion-recruitment-list';
+import { Button } from '~/components/button';
+import useInfiniteAccompanies from '~/hooks/infinite/useInfiniteAccompanies';
+
 import MainFilter from './_components/MainFilter';
 import MobileFilter from './_components/MobileFilter';
 import StyledCarousel from './_components/StyledCarousel';
 
 const Page = () => {
+  const [isMoreCompanionInfinite, setIsMoreCompanionInfinite] = useState(false);
+  const {
+    data: accompaniesData,
+    fetchNextPage: fetchNextAccompaniesPage,
+    hasNextPage: hasNextPageCompanion,
+  } = useInfiniteAccompanies('');
+
   return (
     <div>
       <StyledCarousel />
       <div className="flex w-full border">
         <MainFilter />
-        <div className=" grid w-full grid-cols-3 gap-8 border p-8 sm:grid-cols-1 mainmd:grid-cols-2">
-          {Array.from({ length: 100 }).map((_, index) => (
-            <DummyCard key={index} />
-          ))}
-        </div>
+        {accompaniesData && (
+          <CompanionRecruitmentList
+            data={accompaniesData}
+            isInfinite={isMoreCompanionInfinite}
+            hasNextPage={hasNextPageCompanion}
+            handleFetchNextPage={() => fetchNextAccompaniesPage()}
+          />
+        )}
       </div>
+      {!isMoreCompanionInfinite && (
+        <Button
+          variant="outline"
+          onClick={() => setIsMoreCompanionInfinite(true)}
+          disabled={isMoreCompanionInfinite}
+        >
+          동행 모집 더보기
+        </Button>
+      )}
       <MobileFilter />
     </div>
   );
