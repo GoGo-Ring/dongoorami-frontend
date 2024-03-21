925059586690ec81849a592b258ea489238baa97
ienrum <azang0506@gmail.com>
2024-03-21 14:07:06 +0900

    Refactor: section 로직을 페이지로 이동

--- app/recruitment/[id]/page.tsx
+++ app/recruitment/[id]/page.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { ApplySection } from '~/app/recruitment/[id]/_components/apply';
+import { ApplyForm } from '~/app/recruitment/[id]/_components/apply';
 import ApplyList from '~/app/recruitment/[id]/_components/apply/list';
 import { CommentSection } from '~/app/recruitment/[id]/_components/comment';
 import Field from '~/app/recruitment/[id]/_components/field';
@@ -11,7 +11,10 @@ import Profile from '~/app/recruitment/[id]/_components/profile';
 import Section from '~/app/recruitment/[id]/_components/section';
 import Error from '~/app/recruitment/[id]/error';
 import Loading from '~/app/recruitment/[id]/loading';
+import useMutationCloseCompanyStatus from '~/hooks/mutations/useMutationCloseCompanyStatus';
+import useMutationCreateCompanyComment from '~/hooks/mutations/useMutationCreateCompanyComment';
 import useMutationDeleteCompanyPost from '~/hooks/mutations/useMutationDeleteCompanyPost';
+import useFetchComments from '~/hooks/queries/useFetchComments';
 import useFetchCompanionPost from '~/hooks/queries/useFetchCompanionPost';
 
 interface Params {
@@ -24,6 +27,13 @@ interface Props {
 
 const Page = ({ params }: Props) => {
   const { mutate: mutateDeletePost } = useMutationDeleteCompanyPost();
+  const { mutate: createApplyComment, isPending: isCreatePending } =
+    useMutationCreateCompanyComment(params.id);
+  const { mutate: createCloseStatus } = useMutationCloseCompanyStatus(
+    params.id,
+  );
+
+  const { data: comments, isFetching } = useFetchComments(params.id);
   const { data, isPending, isError, error, refetch } = useFetchCompanionPost(
     params.id,
     true,
@@ -40,9 +50,18 @@ const Page = ({ params }: Props) => {
     );
   }
 
-  if (!data || isPending) {
+  if (!data || isPending || isFetching) {
     return <Loading />;
   }
+
+  const confirmedApplies = comments.filter(
+    ({ isAccompanyConfirmedComment }) => isAccompanyConfirmedComment,
+  );
+
+  const isApplied = comments.some(
+    ({ memberProfile: { currentMember }, isAccompanyApplyComment }) =>
+      currentMember && isAccompanyApplyComment,
+  );
 
   const {
     status,
@@ -50,6 +69,7 @@ const Page = ({ params }: Props) => {
     viewCount,
     memberProfile,
     images,
+    concertName,
     title,
     region,
     totalPeople,
@@ -63,6 +83,18 @@ const Page = ({ params }: Props) => {
     isWish,
     waitingCount,
   } = data;
+
+  const isWriter = memberProfile.currentMember;
+
+  const handleApply = () => {
+    confirm('신청하시겠습니까? 신청 취소는 불가능합니다.') &&
+      createApplyComment();
+  };
+
+  const handleClose = () => {
+    confirm('모집을 마감하시겠습니까? 모집 마감 취소는 불가능합니다.') &&
+      createCloseStatus();
+  };
 
   return (
     <div className="flex flex-col gap-8 py-8">
@@ -81,20 +113,22 @@ const Page = ({ params }: Props) => {
             isWish={isWish}
           />
         </div>
-        <div className="divide-x-2 self-end text-base font-medium text-gray-400">
-          <Link className="px-1" href={`/recruitment/new?id=${params.id}`}>
-            수정
-          </Link>
-          <Link
-            className="px-1"
-            href="/"
-            onClick={() => {
-              mutateDeletePost({ accompanyPostId: params.id });
-            }}
-          >
-            삭제
-          </Link>
-        </div>
+        {isWriter && (
+          <div className="divide-x-2 self-end text-base font-medium text-gray-400">
+            <Link className="px-1" href={`/recruitment/new?id=${params.id}`}>
+              수정
+            </Link>
+            <Link
+              className="px-1"
+              href="/"
+              onClick={() => {
+                mutateDeletePost({ accompanyPostId: params.id });
+              }}
+            >
+              삭제
+            </Link>
+          </div>
+        )}
       </div>
       {images.map((image, index) => (
         <div className="flex justify-center" key={index}>
@@ -104,7 +138,7 @@ const Page = ({ params }: Props) => {
 
       <Section>
         <div className="flex w-full flex-col">
-          <Field label="공연명" value={title} />
+          <Field label="공연명" value={concertName} />
           <Field label="지역" value={region} />
           <Field label="인원수" value={totalPeople} />
           <Field label="공연 날짜" value={`${startDate}~${endDate}`} />
@@ -117,11 +151,19 @@ const Page = ({ params }: Props) => {
         </div>
       </Section>
 
-      <ApplySection
-        accompanyPostId={params.id}
-        isWriter={memberProfile.currentMember}
-        status={status}
-      />
+      <Section className="flex items-center md:flex-nowrap">
+        <h2 className="h-full w-1/2 text-xl font-semibold">
+          신청자 목록 ({`${confirmedApplies.length}/${totalPeople}`}명)
+        </h2>
+        <ApplyForm
+          isDisabled={
+            isPending || isCreatePending || status === '모집 완료' || isApplied
+          }
+          className="flex sm:w-full"
+          content={isWriter ? '모집 마감' : '신청하기'}
+          handleMutate={isWriter ? handleClose : handleApply}
+        />
+      </Section>
 
       <Section>{data?.content}</Section>
 
