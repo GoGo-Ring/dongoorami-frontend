1da7d3a1be5949a97311f2b7fa94d12fcdffbc72
yeonkyoung <keep.ykk@gmail.com>
2024-03-22 13:12:50 +0900

    Feat: 특정 페이지에서 동행 구함 api 리스트를 호출하는 타입 추가

--- apis/scheme/performance.ts
+++ apis/scheme/performance.ts
@@ -1,3 +1,5 @@
+import { PerformanceRecruitment } from './accompany';
+
 export type StatusType = '공연 예정' | '공연 중' | '공연 종료';
 
 interface Performance {
@@ -62,4 +64,14 @@ export interface ConcertReview {
 export interface ConcertReviewList {
   hasNext: boolean;
   concertReviewGetResponses: ConcertReview[];
+}
+
+export interface AccompaniesPostsConcerts {
+  concertId: string;
+  size?: number;
+}
+
+export interface AccompanyPostConcertResponses {
+  hasNext: false;
+  accompanyPostConcertResponses: PerformanceRecruitment[];
 }
