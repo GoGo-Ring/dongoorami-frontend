adf5ef8f12e407bd7b6595fa9d58f7e545f91b82
leejaejoon <jaejoon0721@ajou.ac.kr>
2024-03-23 16:23:38 +0900

    Feat: 비로그인 시 로그인 페이지로 리다이렉션 추가

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { ChangeEvent, useState } from 'react';
 import { toast } from 'sonner';
 
@@ -21,6 +22,7 @@ import Wishes from './_components/wishes';
 import { TABS_VALUE } from './constants';
 
 const Page = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
   const [isEdit, setIsEdit] = useState(false);
 
   const { data: member } = useFetchMember();
@@ -32,6 +34,8 @@ const Page = ({ params }: { params: { id: string } }) => {
   const { mutate: mutateUpdateMember } = useMutationUpdateMember();
 
   if (!member) {
+    router.push('/login');
+
     return null;
   }
 
