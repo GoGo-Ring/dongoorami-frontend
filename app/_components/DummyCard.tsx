9f37dce9c4e45d25d4847afc839299664e32ac0d
yeonkyoung <keep.ykk@gmail.com>
2024-03-23 21:52:44 +0900

    Feat: [#171] 동행 구함 카드 컴포넌트 클릭시 상세페이지로 이동하는 기능 추가 (#172)
    * Chore: 파일명 변경
    
    * Feat: Link 추가, 동행 구인 페이지로 이동
    
    * Chore: import 파일명 변경
    
    * Refactor: mock 데이터가 있는 id로 수정

--- app/_components/DummyCard.tsx
+++ app/_components/DummyCard.tsx
@@ -1,10 +1,10 @@
-import CompanionRecruitmentCard from './CompanionRecruitmentCard';
+import CompanionRecruitmentCard from './companion-recruitment-card';
 
 export const DummyCard = () => {
   return (
     <div className="flex items-center justify-center">
       <CompanionRecruitmentCard
-        id={11}
+        id={12}
         title="동행 구해요"
         writer="작성자"
         createdAt={'2024-03-18'}
