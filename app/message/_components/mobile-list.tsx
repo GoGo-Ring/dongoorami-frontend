734d30f65ba63a7692af5df05a70c93f989e0438
seongminn <berry9901@gmail.com>
2024-03-30 16:29:29 +0900

    Chore: 모바일 더미 제거 및 링크 연결

--- app/message/_components/mobile-list.tsx
+++ app/message/_components/mobile-list.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import useFetchMessage from '~/hooks/queries/useFetchMessage';
 import { getDate } from '~/utils/dateFormatter';
 
@@ -9,30 +11,25 @@ const MobileMessage = () => {
   return (
     <ul className="flex flex-col gap-3 md:hidden lg:hidden">
       {data?.messageResponses.map(message => (
-        <li
-          key={message.id}
-          className="flex items-center justify-between gap-4 border-b px-2 pb-2"
-        >
-          <div className="flex flex-col">
-            <h3>{message.partner.id}</h3>
-            <p className="text-gray-400">
-              {getDate(new Date(message.createdAt), 'yyyy.mm.dd')}
-            </p>
-            <p className="line-clamp-1 text-ellipsis">{message.content}</p>
-          </div>
-          <span className="text-nowrap">{getRead(!message.hasUnRead)}</span>
+        <li key={message.id}>
+          <Link
+            className="flex items-center justify-between gap-4 border-b px-2 pb-2"
+            href={{
+              pathname: '/message/write',
+              query: { userId: message.partner.id },
+            }}
+          >
+            <div className="flex flex-col">
+              <h3>{message.partner.id}</h3>
+              <p className="text-gray-400">
+                {getDate(new Date(message.createdAt), 'yyyy.mm.dd')}
+              </p>
+              <p className="line-clamp-1 text-ellipsis">{message.content}</p>
+            </div>
+            <span className="text-nowrap">{getRead(!message.hasUnRead)}</span>
+          </Link>
         </li>
       ))}
-      <li className="flex items-center justify-between gap-4 border-b px-2 pb-2">
-        <div className="flex flex-col">
-          <h3>유저입니다.</h3>
-          <p className="text-gray-400">{getDate(new Date(), 'yyyy.mm.dd')}</p>
-          <span className="line-clamp-1 text-ellipsis">
-            내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.내용입니다.
-          </span>
-        </div>
-        <span className="text-nowrap">{getRead(false)}</span>
-      </li>
     </ul>
   );
 };
